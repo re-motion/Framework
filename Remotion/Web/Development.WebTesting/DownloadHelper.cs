7e0ac113cdf6459965b08b2c56415e0d60350735
Dominik Rauch <dominik.rauch@rubicon.eu>
2016-03-17 16:22:49 +0000

    Increased timeouts when using IE download bar.

--- Remotion/Web/Development.WebTesting/DownloadHelper.cs
+++ Remotion/Web/Development.WebTesting/DownloadHelper.cs
@@ -81,7 +81,9 @@ namespace Remotion.Web.Development.WebTesting
       if (_browserConfiguration.BrowserIsInternetExplorer())
       {
         Thread.Sleep (1500); // do not press too fast, IE-security in place
-        SendKeys.SendWait ("{F6}{TAB}{ENTER}");
+        SendKeys.SendWait ("{F6}");
+        Thread.Sleep (1000);
+        SendKeys.SendWait ("{TAB}{ENTER}");
       }
 
       var errorMessage = string.Format ("File download of '{0}' failed.", _fileName);
@@ -93,7 +95,9 @@ namespace Remotion.Web.Development.WebTesting
       if (_browserConfiguration.BrowserIsInternetExplorer())
       {
         Thread.Sleep (1500); // do not press too fast, IE-security in place
-        SendKeys.SendWait ("{F6}{TAB}{TAB}{TAB}{ENTER}");
+        SendKeys.SendWait ("{F6}");
+        Thread.Sleep (1000);
+        SendKeys.SendWait("{TAB}{TAB}{TAB}{ENTER}");
       }
 
       s_log.DebugFormat ("Download to '{0}' successfully completed", _fullFilePath);
