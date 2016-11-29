bbb86eff7325401689f7669737553186cbac5ac7
Michael Ketting <michael.ketting@rubicon.eu>
2016-11-29 09:32:34 +0000

    Update tests for WebTreeView to use the pipe-character in the item ID. (RM-6665)

--- Remotion/ObjectBinding/Web.Test/SingleTestTreeView.aspx.cs
+++ Remotion/ObjectBinding/Web.Test/SingleTestTreeView.aspx.cs
@@ -73,55 +73,57 @@ namespace OBWTest
 
       WebTreeNodeCollection nodes;
 
+      // Use '|' in the tree node IDs to simulate serialized ObjectIDs and detect conflicts.
+
       nodes = WebTreeView.Nodes;
       nodes.Add (
-          new WebTreeNode ("node0", "Node 0", "Hello", new IconInfo ("Images/Remotion.ObjectBinding.Sample.Job.gif", "Icon", "ToolTip", Unit.Pixel (16), Unit.Pixel (16))));
-      nodes.Add (new WebTreeNode ("node1", "Node 1"));
-      nodes.Add (new WebTreeNode ("node2", "Node 2"));
-      nodes.Add (new WebTreeNode ("node3", "Node 3"));
-      nodes.Add (new WebTreeNode ("node4", "Node 4"));
+          new WebTreeNode ("node0|id", "Node 0", "Hello", new IconInfo ("Images/Remotion.ObjectBinding.Sample.Job.gif", "Icon", "ToolTip", Unit.Pixel (16), Unit.Pixel (16))));
+      nodes.Add (new WebTreeNode ("node1|id", "Node 1"));
+      nodes.Add (new WebTreeNode ("node2|id", "Node 2"));
+      nodes.Add (new WebTreeNode ("node3|id", "Node 3"));
+      nodes.Add (new WebTreeNode ("node4|id", "Node 4"));
 
       nodes = ((WebTreeNode) WebTreeView.Nodes[0]).Children;
-      nodes.Add (new WebTreeNode ("node00", "Node 0-0", "Images/Remotion.ObjectBinding.Sample.Job.gif"));
-      nodes.Add (new WebTreeNode ("node01", "Node 0-1"));
-      nodes.Add (new WebTreeNode ("node02", "Node 0-2"));
-      nodes.Add (new WebTreeNode ("node03", "Node 0-3"));
+      nodes.Add (new WebTreeNode ("node00|id", "Node 0-0", "Images/Remotion.ObjectBinding.Sample.Job.gif"));
+      nodes.Add (new WebTreeNode ("node01|id", "Node 0-1"));
+      nodes.Add (new WebTreeNode ("node02|id", "Node 0-2"));
+      nodes.Add (new WebTreeNode ("node03|id", "Node 0-3"));
       ((WebTreeNode) WebTreeView.Nodes[0]).IsEvaluated = true;
 
       nodes = ((WebTreeNode) ((WebTreeNode) WebTreeView.Nodes[0]).Children[0]).Children;
-      nodes.Add (new WebTreeNode ("node000", "Node 0-0-0"));
-      nodes.Add (new WebTreeNode ("node001", "Node 0-0-1", "Hello", new IconInfo ("Images/Remotion.ObjectBinding.Sample.Job.gif")));
+      nodes.Add (new WebTreeNode ("node000|id", "Node 0-0-0"));
+      nodes.Add (new WebTreeNode ("node001|id", "Node 0-0-1", "Hello", new IconInfo ("Images/Remotion.ObjectBinding.Sample.Job.gif")));
       nodes.Add (
-          new WebTreeNode ("node002", "Node 0-0-2", "Hello", new IconInfo ("Images/Remotion.ObjectBinding.Sample.Job.gif", "Icon", null, Unit.Pixel (16), Unit.Pixel (16))));
-      nodes.Add (new WebTreeNode ("node003", "Node 0-0-3", "Images/Remotion.ObjectBinding.Sample.Job.gif"));
+          new WebTreeNode ("node002|id", "Node 0-0-2", "Hello", new IconInfo ("Images/Remotion.ObjectBinding.Sample.Job.gif", "Icon", null, Unit.Pixel (16), Unit.Pixel (16))));
+      nodes.Add (new WebTreeNode ("node003|id", "Node 0-0-3", "Images/Remotion.ObjectBinding.Sample.Job.gif"));
       ((WebTreeNode) ((WebTreeNode) WebTreeView.Nodes[0]).Children[0]).IsEvaluated = true;
 
       nodes = ((WebTreeNode) WebTreeView.Nodes[3]).Children;
-      nodes.Add (new WebTreeNode ("node30", "Node 3-0"));
-      nodes.Add (new WebTreeNode ("node31", "Node 3-1"));
-      nodes.Add (new WebTreeNode ("node32", "Node 3-2"));
-      nodes.Add (new WebTreeNode ("node33", "Node 3-3"));
+      nodes.Add (new WebTreeNode ("node30|id", "Node 3-0"));
+      nodes.Add (new WebTreeNode ("node31|id", "Node 3-1"));
+      nodes.Add (new WebTreeNode ("node32|id", "Node 3-2"));
+      nodes.Add (new WebTreeNode ("node33|id", "Node 3-3"));
       ((WebTreeNode) WebTreeView.Nodes[3]).IsEvaluated = true;
 
       nodes = ((WebTreeNode) ((WebTreeNode) WebTreeView.Nodes[3]).Children[3]).Children;
-      nodes.Add (new WebTreeNode ("node330", "Node 3-3-0"));
-      nodes.Add (new WebTreeNode ("node331", "Node 3-3-1"));
-      nodes.Add (new WebTreeNode ("node332", "Node 3-3-2"));
-      nodes.Add (new WebTreeNode ("node333", "Node 3-3-3"));
+      nodes.Add (new WebTreeNode ("node330|id", "Node 3-3-0"));
+      nodes.Add (new WebTreeNode ("node331|id", "Node 3-3-1"));
+      nodes.Add (new WebTreeNode ("node332|id", "Node 3-3-2"));
+      nodes.Add (new WebTreeNode ("node333|id", "Node 3-3-3"));
       ((WebTreeNode) ((WebTreeNode) WebTreeView.Nodes[3]).Children[3]).IsEvaluated = true;
 
       nodes = ((WebTreeNode) WebTreeView.Nodes[2]).Children;
-      nodes.Add (new WebTreeNode ("node20", "Node 2-0"));
-      nodes.Add (new WebTreeNode ("node21", "Node 2-1"));
-      nodes.Add (new WebTreeNode ("node22", "Node 2-2"));
-      nodes.Add (new WebTreeNode ("node23", "Node 2-3"));
+      nodes.Add (new WebTreeNode ("node20|id", "Node 2-0"));
+      nodes.Add (new WebTreeNode ("node21|id", "Node 2-1"));
+      nodes.Add (new WebTreeNode ("node22|id", "Node 2-2"));
+      nodes.Add (new WebTreeNode ("node23|id", "Node 2-3"));
       ((WebTreeNode) WebTreeView.Nodes[2]).IsEvaluated = true;
 
       nodes = ((WebTreeNode) WebTreeView.Nodes[4]).Children;
-      nodes.Add (new WebTreeNode ("node40", "Node 4-0"));
-      nodes.Add (new WebTreeNode ("node41", "Node 4-1"));
-      nodes.Add (new WebTreeNode ("node42", "Node 4-2"));
-      nodes.Add (new WebTreeNode ("node43", "Node 4-3"));
+      nodes.Add (new WebTreeNode ("node40|id", "Node 4-0"));
+      nodes.Add (new WebTreeNode ("node41|id", "Node 4-1"));
+      nodes.Add (new WebTreeNode ("node42|id", "Node 4-2"));
+      nodes.Add (new WebTreeNode ("node43|id", "Node 4-3"));
       ((WebTreeNode) WebTreeView.Nodes[4]).IsEvaluated = true;
 
       WebTreeView.SetEvaluateTreeNodeDelegate (new EvaluateWebTreeNode (EvaluateTreeNode));
