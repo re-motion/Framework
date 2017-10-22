9d349a7fbfeb5bdf7b77c75229d10852c26a700e
re-motion Build User <FX-Team@rubicon.eu>
2017-10-22 23:00:43 +0000

    Comitting AssemblyInfos for local build after setting version to '1.17.19.20000'.

--- Remotion/Mixins/AssemblyInfoShared.cs
+++ Remotion/Mixins/AssemblyInfoShared.cs
@@ -27,7 +27,7 @@ using System.Reflection;
 [assembly: AssemblyProduct("re-motion Framework: re-mix (www.re-motion.org)")]
 [assembly: AssemblyCopyright ("Copyright (c) rubicon IT GmbH, www.rubicon.eu - Licensed under GNU LGPL 2.1 (or later)")]
 [assembly: AssemblyTrademark ("")]
-[assembly: AssemblyInformationalVersion ("1.17.18.20000+Debug")]
+[assembly: AssemblyInformationalVersion ("1.17.19.20000+Debug")]
 
 //
 // Version information for an assembly consists of the following four values:
@@ -40,5 +40,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.17.18.20000")]
-[assembly: AssemblyFileVersion ("1.17.18.20000")]
+[assembly: AssemblyVersion("1.17.19.20000")]
+[assembly: AssemblyFileVersion ("1.17.19.20000")]
