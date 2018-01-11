d5b56711b2a1946184fac9a4bc6609decd7b2b7c
re-motion Build User <FX-Team@rubicon.eu>
2018-01-11 11:19:27 +0000

    Tagging build version 1.16.31

--- Remotion/Mixins/AssemblyInfoShared.cs
+++ Remotion/Mixins/AssemblyInfoShared.cs
@@ -28,7 +28,7 @@ using System.Reflection;
 [assembly: AssemblyProduct("re-motion Framework: re-mix (www.re-motion.org)")]
 [assembly: AssemblyCopyright ("Copyright (c) rubicon IT GmbH, www.rubicon.eu - Licensed under GNU LGPL 2.1 (or later)")]
 [assembly: AssemblyTrademark ("")]
-[assembly: AssemblyInformationalVersion ("1.16.30.20000+Debug")]
+[assembly: AssemblyInformationalVersion ("1.16.31.10000+Debug.RTM.SVNRevision-32152")]
 
 //
 // Version information for an assembly consists of the following four values:
@@ -41,5 +41,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.16.30.20000")]
-[assembly: AssemblyFileVersion ("1.16.30.20000")]
+[assembly: AssemblyVersion("1.16.31.10000")]
+[assembly: AssemblyFileVersion ("1.16.31.10000")]
