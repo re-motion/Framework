0c0a32b387baa9546d8d6862a609a056d09f6a20
Michael Ketting <michael.ketting@rubicon.eu>
2018-05-29 08:43:23 +0000

    Renamed SecurityManagerCurrentTenantControl to SecurityManagerUserContextControl (RM-6976)

--- SecurityManager/Clients.Web/UI/CurrentTenantControl.obsolete.cs
+++ SecurityManager/Clients.Web/UI/CurrentTenantControl.obsolete.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace Remotion.SecurityManager.Clients.Web.UI
 {
-  [Obsolete ("Use SecurityManagerUserContextControl instead. (Version: 1.19.4)")]
+  [Obsolete ("Use SecurityManagerUserContextControl instead. (Version: 1.19.4)", true)]
   public sealed class CurrentTenantControl
   {
   }
