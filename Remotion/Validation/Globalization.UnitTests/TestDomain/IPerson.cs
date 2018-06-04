a86484a602e1be92353f790c847147919f8a3795
Michael Ketting <michael.ketting@rubicon.eu>
2018-06-04 12:53:22 +0000

    Replaced JetBrains.Annotations with official DLL instead of using re-motion-compiled annotations. (RM-6962)

--- Remotion/Validation/Globalization.UnitTests/TestDomain/IPerson.cs
+++ Remotion/Validation/Globalization.UnitTests/TestDomain/IPerson.cs
@@ -15,7 +15,7 @@
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
 using System;
-using JetBrains.Annotations;
+using Remotion.Validation.Attributes.Validation;
 
 namespace Remotion.Validation.Globalization.UnitTests.TestDomain
 {
