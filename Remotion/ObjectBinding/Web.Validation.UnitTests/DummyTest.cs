dedc628f251750d2b31460632f3902ba5d20f076
Michael Ketting <michael.ketting@rubicon.eu>
2020-04-23 13:36:35 +0000

    Moved implementation of validation error dispatching from Remotion.ObjectBinding.Web.Validation to Remotion.ObjectBinding.Web. (fixed tests) (RM-6056)

---
+++ Remotion/ObjectBinding/Web.Validation.UnitTests/DummyTest.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Remotion.ObjectBinding.Web.Validation.UnitTests
+{
+  [TestFixture]
+  public class DummyTest
+  {
+    [Test]
+    [Ignore ("Dmummy test to prevent empty test results")]
+    public void IgnoredDummyTest ()
+    {
+    }
+  }
+}
