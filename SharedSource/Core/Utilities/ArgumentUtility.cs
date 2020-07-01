4d2f6eca9adc67098f1d62fea29783d8a143f9cd
Marcel Holle <marcel.holle@rubicon.eu>
2020-07-01 07:55:24 +0000

    Inlined CheckNotNull call in CheckNotNullOrEmpty. (RM-7444)

--- SharedSource/Core/Utilities/ArgumentUtility.cs
+++ SharedSource/Core/Utilities/ArgumentUtility.cs
@@ -90,7 +90,9 @@ namespace Remotion.Utilities
         [InvokerParameterName] string argumentName,
         [AssertionCondition (AssertionConditionType.IS_NOT_NULL)] string actualValue)
     {
-      CheckNotNull (argumentName, actualValue);
+      if (actualValue == null)
+        throw new ArgumentNullException (argumentName);
+
       if (actualValue.Length == 0)
         throw CreateArgumentEmptyException (argumentName);
 
