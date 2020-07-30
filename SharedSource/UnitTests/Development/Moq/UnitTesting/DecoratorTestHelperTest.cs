9573224caa253268463eb2107e02162c66236d4d
Stefan Ilic <stefan.ilic@rubicon.eu>
2020-07-30 08:40:34 +0000

    Added Moq-compatible DecoratorTestHelper. (RM-7461)

---
+++ SharedSource/UnitTests/Development/Moq/UnitTesting/DecoratorTestHelperTest.cs
@@ -0,0 +1,138 @@
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// See the NOTICE file distributed with this work for additional information
+// regarding copyright ownership.  rubicon licenses this file to you under
+// the Apache License, Version 2.0 (the "License"); you may not use this
+// file except in compliance with the License.  You may obtain a copy of the
+// License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+// License for the specific language governing permissions and limitations
+// under the License.
+//
+
+using System;
+using Moq;
+using NUnit.Framework;
+using Remotion.Development.Moq.UnitTesting;
+
+#nullable enable
+
+namespace Remotion.UnitTests.Development.Moq.UnitTesting
+{
+  [TestFixture]
+  public class DecoratorTestHelperTest
+  {
+    private DecoratorTestHelper<IMyInterface> _helperForDecorator = default!;
+    private DecoratorTestHelper<IMyInterface> _helperForNonDelegatingDecorator = default!;
+    private DecoratorTestHelper<IMyInterface> _helperForFaultyDecorator = default!;
+
+    [SetUp]
+    public void SetUp ()
+    {
+      _helperForDecorator = CreateDecoratorTestHelper (
+          inner => inner.Get(),
+          (inner, s) => inner.Do (s));
+
+      _helperForNonDelegatingDecorator = CreateDecoratorTestHelper (inner => "Abc", (inner, s) => { });
+      _helperForFaultyDecorator = CreateDecoratorTestHelper (
+          inner =>
+          {
+            inner.Get();
+            return "faulty";
+          },
+          (inner, s) => inner.Do ("faulty"));
+    }
+
+    [Test]
+    public void CheckDelegation_Func ()
+    {
+      Assert.That (() => _helperForDecorator.CheckDelegation (d => d.Get(), "Abc"), Throws.Nothing);
+      Assert.That (
+          () => _helperForNonDelegatingDecorator.CheckDelegation (d => d.Get(), "Abc"),
+          Throws.TypeOf<MockException>().And.Message.Matches (
+              @"Mock<DecoratorTestHelperTest\.IMyInterface:\d+>:\s*This mock failed verification due to the following:\s*DecoratorTestHelperTest\.IMyInterface d => d\.Get\(\):\s*This setup was not matched\."));
+      Assert.That (
+          () => _helperForFaultyDecorator.CheckDelegation (d => d.Get(), "Abc"),
+          Throws.TypeOf<AssertionException>().And.Message.StartsWith ("  Expected string length 3 but was 6. Strings differ at index 0."));
+    }
+
+    [Test]
+    public void CheckDelegation_Func_MultipleCallsForSameMock ()
+    {
+      Assert.That (() => _helperForDecorator.CheckDelegation (d => d.Get(), "Abc"), Throws.Nothing);
+      Assert.That (() => _helperForDecorator.CheckDelegation (d => d.Get(), "Abc"), Throws.Nothing);
+    }
+
+    [Test]
+    public void CheckDelegation_Action ()
+    {
+      Assert.That (() => _helperForDecorator.CheckDelegation (d => d.Do ("Abc")), Throws.Nothing);
+      Assert.That (
+          () => _helperForNonDelegatingDecorator.CheckDelegation (d => d.Do ("Abc")),
+          Throws.TypeOf<MockException>().And.Message.Matches (
+              @"Mock<DecoratorTestHelperTest.IMyInterface:\d+>:\s*This mock failed verification due to the following:\s*DecoratorTestHelperTest\.IMyInterface d => d\.Do\(""Abc""\):"));
+      Assert.That (
+          () => _helperForFaultyDecorator.CheckDelegation (d => d.Do ("Abc")),
+          Throws.TypeOf<MockException>().And.Message.Matches (
+              @"DecoratorTestHelperTest.IMyInterface.Do\(""faulty""\) invocation failed with mock behavior Strict.\s*All invocations on the mock must have a corresponding setup\."));
+    }
+
+    [Test]
+    public void CheckDelegation_Action_MultipleCallsForSameMock ()
+    {
+      Assert.That (() => _helperForDecorator.CheckDelegation (d => d.Do ("Abc")), Throws.Nothing);
+      Assert.That (() => _helperForDecorator.CheckDelegation (d => d.Do ("Abc")), Throws.Nothing);
+      Assert.That (() => _helperForDecorator.CheckDelegation (d => d.Do ("Abc")), Throws.Nothing);
+    }
+
+    [Test]
+    public void CheckDelegation_Mixed_MultipleCallsForSameMock ()
+    {
+      Assert.That (() => _helperForDecorator.CheckDelegation (d => d.Do ("Abc")), Throws.Nothing);
+      Assert.That (() => _helperForDecorator.CheckDelegation (d => d.Get(), "test"), Throws.Nothing);
+    }
+
+    private DecoratorTestHelper<IMyInterface> CreateDecoratorTestHelper (Func<IMyInterface, string> getMethod, Action<IMyInterface, string> doMethod)
+    {
+      var innerMock = new Mock<IMyInterface> (MockBehavior.Strict);
+      var decorator = new Decorator (innerMock.Object, getMethod, doMethod);
+
+      return new DecoratorTestHelper<IMyInterface> (decorator, innerMock);
+    }
+
+    public interface IMyInterface
+    {
+      string Get ();
+      void Do (string s);
+    }
+
+    private class Decorator : IMyInterface
+    {
+      private readonly IMyInterface _inner;
+      private readonly Func<IMyInterface, string> _getMethod;
+      private readonly Action<IMyInterface, string> _doMethod;
+
+      public Decorator (IMyInterface inner, Func<IMyInterface, string> getMethod, Action<IMyInterface, string> doMethod)
+      {
+        _inner = inner;
+        _getMethod = getMethod;
+        _doMethod = doMethod;
+      }
+
+      public string Get ()
+      {
+        return _getMethod (_inner);
+      }
+
+      public void Do (string s)
+      {
+        _doMethod (_inner, s);
+      }
+    }
+  }
+}
