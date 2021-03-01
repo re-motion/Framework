020f9f70ff92b9aa331c00d1cc664fcc7c12ed0a
Widauer, Patrick <patrick.widauer@rubicon.eu>
2021-03-01 10:31:03 +0100

    review: convert TextBoxStyle into a class

--- Remotion/ObjectBinding/Web.ClientScript/Scripts/TextBoxStyle.ts
+++ Remotion/ObjectBinding/Web.ClientScript/Scripts/TextBoxStyle.ts
@@ -15,36 +15,44 @@
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
 
-// Cannot detect paste operations.
-function TextBoxStyle_OnKeyDown (textBox: HTMLInputElement, length: number)
+class TextBoxStyle
 {
-  if (textBox.disabled)
-    return true;
-  
-  // TODO: Better provide the event via parameter instead of ambient usage
-  function typeOverride <T>(value: unknown): asserts value is T {};
-  typeOverride<KeyboardEvent>(event);
-  
-  // TODO: maybe don't use keyCode and use key instead?
-  var isInsertDelete = event.keyCode == 45 || event.keyCode == 46;
-  var isCursor = event.keyCode >= 37 && event.keyCode <= 40; // Left, Top, Right, Buttom
-  var isPagePosition = event.keyCode >= 33 && event.keyCode <= 36; // Home, End, PageUp, PageDown
-  var isControlCharacter = 
-         event.keyCode < 32 
-      || isInsertDelete 
-      || isCursor 
-      || isPagePosition
-      || event.ctrlKey 
-      || event.altKey;
-  var isLineFeed = event.keyCode == 10;
-  var isCarriageReturn = event.keyCode == 13;
-  
-  
-  if (isControlCharacter && ! (isLineFeed || isCarriageReturn))
-    return true;
-  
-  if (textBox.value.length >= length) 
-    return false;
+  // Cannot detect paste operations.
+  public static OnKeyDown (textBox: HTMLInputElement, length: number): boolean
+  {
+    if (textBox.disabled)
+      return true;
     
-  return true;
+    // TODO: Better provide the event via parameter instead of ambient usage
+    function typeOverride <T>(value: unknown): asserts value is T {};
+    typeOverride<KeyboardEvent>(event);
+    
+    // TODO: maybe don't use keyCode and use key instead?
+    var isInsertDelete = event.keyCode == 45 || event.keyCode == 46;
+    var isCursor = event.keyCode >= 37 && event.keyCode <= 40; // Left, Top, Right, Buttom
+    var isPagePosition = event.keyCode >= 33 && event.keyCode <= 36; // Home, End, PageUp, PageDown
+    var isControlCharacter = 
+          event.keyCode < 32 
+        || isInsertDelete 
+        || isCursor 
+        || isPagePosition
+        || event.ctrlKey 
+        || event.altKey;
+    var isLineFeed = event.keyCode == 10;
+    var isCarriageReturn = event.keyCode == 13;
+    
+    
+    if (isControlCharacter && ! (isLineFeed || isCarriageReturn))
+      return true;
+    
+    if (textBox.value.length >= length) 
+      return false;
+      
+    return true;
+  }
+}
+
+function TextBoxStyle_OnKeyDown (textBox: HTMLInputElement, length: number): boolean
+{
+  return TextBoxStyle.OnKeyDown (textBox, length);
 }
