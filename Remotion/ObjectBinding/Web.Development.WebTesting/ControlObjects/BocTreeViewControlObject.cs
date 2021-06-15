f4b510118a5af4b38c9d8a3a169d2dd58d32ecff
Marcel Holle <marcel.holle@rubicon.eu>
2021-06-15 11:27:29 +0200

    RM-7850 Add obsolete warning, enhance documentation
    (cherry picked from commit 64863ba886a52e1d5b1a4bb70d9322d0888b5bc6)

--- Remotion/ObjectBinding/Web.Development.WebTesting/ControlObjects/BocTreeViewControlObject.cs
+++ Remotion/ObjectBinding/Web.Development.WebTesting/ControlObjects/BocTreeViewControlObject.cs
@@ -44,8 +44,9 @@ namespace Remotion.ObjectBinding.Web.Development.WebTesting.ControlObjects
     }
 
     /// <summary>
-    /// Returns the tree's root node.
+    /// Returns the tree's first root node.
     /// </summary>
+    [Obsolete ("This method is equivalent to .GetNode().WithIndex (1), which should be used instead. (Version 2.29.0)", false)]
     public BocTreeViewNodeControlObject GetRootNode ()
     {
       return _metaRootNode.GetNode().WithIndex (1);
