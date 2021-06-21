2e2a36e9a11259b123f8692787b199c77504da01
Jakob Müllner <jakob.muellner@rubicon.eu>
2021-06-21 08:51:52 +0200

    Update tests in ObjectBinding.Web.Compatibility.UnitTests (manual)

--- Remotion/ObjectBinding/Web.Compatibility.UnitTests/UI/Controls/Validation/CompatibilityBusinessObjectBoundEditableWebControlValidatorConfigurationTest.cs
+++ Remotion/ObjectBinding/Web.Compatibility.UnitTests/UI/Controls/Validation/CompatibilityBusinessObjectBoundEditableWebControlValidatorConfigurationTest.cs
@@ -1,10 +1,7 @@
 ﻿using Moq;
-using Moq.Protected;
 using NUnit.Framework;
 using Remotion.ObjectBinding.Web.UI.Controls;
 using Remotion.ObjectBinding.Web.UI.Controls.Validation;
-using Rhino.Mocks;
-using MockRepository = Rhino.Mocks.MockRepository;
 
 namespace Remotion.ObjectBinding.Web.UnitTests.UI.Controls.Validation
 {
