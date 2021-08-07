4c8f279ce4256c7c999b1af30eeb6de0faaa314f
Michael Ketting <michael.ketting@rubicon.eu>
2021-08-07 18:38:26 +0200

    RM-7879 Add workaround for compatibility issue with Main()-method in .NET 5.0

--- Remotion/Data/DomainObjects.PerformanceTests/Root.cs
+++ Remotion/Data/DomainObjects.PerformanceTests/Root.cs
@@ -34,6 +34,7 @@ namespace Remotion.Data.DomainObjects.PerformanceTests
     {
     }
 
+#if NETFRAMEWORK
     [STAThread]
     public static void Main (string[] args)
     {
@@ -70,6 +71,7 @@ namespace Remotion.Data.DomainObjects.PerformanceTests
       Console.WriteLine ("Tests complete");
       //Console.ReadLine();
     }
+#endif
 
     private static void RunHasRelationChangedTest ()
     {
