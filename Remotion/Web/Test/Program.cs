d32c1d35b6413d31fb5feef1ba1ab05ffc89a180
Patrick Widauer <patrick.widauer@rubicon.eu>
2021-08-11 16:39:07 +0200

    RM-7887 Integrate CoreForms into Web.Test

---
+++ Remotion/Web/Test/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Web.Integration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Remotion.Web.Test
+{
+  public class Program
+  {
+    public static void Main (string[] args)
+    {
+      Console.WriteLine (Environment.CurrentDirectory);
+      LegacyAspNetInitialization.Init ("/", Environment.CurrentDirectory, true);
+
+      CreateHostBuilder (args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder (string[] args)
+    {
+      return Host.CreateDefaultBuilder (args)
+          .ConfigureWebHostDefaults (webBuilder => { webBuilder.UseStartup<Startup>(); });
+    }
+  }
+}
