b493f52c596a3ed6275bba10ea60fa2ab9364c3f
Michael Ketting <michael.ketting@rubicon.eu>
2021-08-28 16:50:07 +0200

    RM-8296 Fix null bug in IncompleteEndPointModicationVirtualCollectionData.GetObject(int index)

---
+++ Remotion/Data/DomainObjects.UnitTests/DataManagement/CollectionData/IncompleteEndPointModificationVirtualCollectionDataTest.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+
+namespace Remotion.Data.DomainObjects.UnitTests.DataManagement.CollectionData
+{
+    [TestFixture]
+    public class IncompleteEndPointModificationVirtualCollectionDataTest
+    {
+        
+    }
+}
