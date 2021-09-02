3949b8cede85960195225f78d2285f48fac1735b
Florian Lautner <florian.lautner@rubicon.eu>
2021-09-02 13:54:51 +0200

    RM-7917 Create styles for new theme for BocEnumValue

--- Remotion/ObjectBinding/Web.UnitTests/UI/Controls/BocEnumValueImplementation/Rendering/BocEnumValueRendererTests/SelectList_BEVRT.cs
+++ Remotion/ObjectBinding/Web.UnitTests/UI/Controls/BocEnumValueImplementation/Rendering/BocEnumValueRendererTests/SelectList_BEVRT.cs
@@ -259,7 +259,7 @@ namespace Remotion.ObjectBinding.Web.UnitTests.UI.Controls.BocEnumValueImplement
       if (string.IsNullOrEmpty (cssClass))
         cssClass = _enumValue.Object.Attributes["class"];
       if (string.IsNullOrEmpty (cssClass))
-        cssClass = renderer.GetCssClassBase(_enumValue.Object);
+        cssClass = "bocEnumValue dropDownList";
 
       Html.AssertAttribute (div, "id", "MyEnumValue");
       Html.AssertAttribute (div, "class", cssClass, HtmlHelperBase.AttributeValueCompareMode.Contains);
