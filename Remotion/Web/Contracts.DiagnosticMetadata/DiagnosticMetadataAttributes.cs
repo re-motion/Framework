ce703827b81ed30eb21c929099f013e418a3095d
Jakob Müllner <jakob.muellner@rubicon.eu>
2021-09-29 11:21:11 +0200

    RM-8019 Support grouping for WebTreeView nodes

--- Remotion/Web/Contracts.DiagnosticMetadata/DiagnosticMetadataAttributes.cs
+++ Remotion/Web/Contracts.DiagnosticMetadata/DiagnosticMetadataAttributes.cs
@@ -40,6 +40,7 @@ namespace Remotion.Web.Contracts.DiagnosticMetadata
     public static readonly string WebTreeViewNumberOfChildren = "data-webtreeview-number-of-children";
     public static readonly string WebTreeViewIsSelectedNode = "data-webtreeview-is-selected-node";
     public static readonly string WebTreeViewIsExpanded = "data-webtreeview-is-expanded";
+    public static readonly string WebTreeViewNodeCategory = "data-webtreeview-node-category";
 
     public static readonly string ValidationErrorIDIndex = "data-validation-error-id-index";
     public static readonly string LabelIDIndex = "data-label-id-index";
