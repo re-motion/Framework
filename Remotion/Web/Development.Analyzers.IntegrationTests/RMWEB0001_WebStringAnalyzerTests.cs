979308e3ac44b7abccb866f9abfe99f84f571eb2
Marcel Holle <marcel.holle@rubicon.eu>
2021-11-23 12:39:20 +0100

    RM-8215 Add WebString/PlainTextString usage analyzer

---
+++ Remotion/Web/Development.Analyzers.IntegrationTests/RMWEB0001_WebStringAnalyzerTests.cs
@@ -0,0 +1,246 @@
+﻿// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// The re-motion Core Framework is free software; you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// re-motion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+//
+using System;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Verifier = Remotion.Web.Development.Analyzers.IntegrationTests.CSharpAnalyzerVerifier<Web.Development.Analyzers.RMWEB0001_WebStringAnalyzer>;
+
+namespace Remotion.Web.Development.Analyzers.IntegrationTests
+{
+  [TestFixture]
+  public class RMWEB0001_WebStringAnalyzerTests
+  {
+    [Test]
+    public async Task StringBuilder_WithWebString_Diagnostic ()
+    {
+      const string input = @"
+using System.Text;
+using Remotion.Web;
+public class A
+{
+  public void Test()
+  {
+    var sb = new StringBuilder();
+    var webString = WebString.CreateFromText(""test"");
+    sb.Append(webString);
+  }
+}";
+
+      var diagnostic = Verifier.Diagnostic()
+          .WithSpan (10, 5, 10, 25)
+          .WithMessage (
+              "'System.Text.StringBuilder.Append(object?)' should not be used with a 'Remotion.Web.WebString' argument. "
+              + "Call '.ToString(WebStringEncoding.HtmlWithTransformedLineBreaks)' on the Remotion.Web.WebString instance instead.");
+      await Verifier.VerifyAnalyzerAsync (input, diagnostic);
+    }
+
+    [Test]
+    public async Task StringBuilder_WithPlainTextString_Diagnostic ()
+    {
+      const string input = @"
+using System.Text;
+using Remotion.Web;
+public class A
+{
+  public void Test()
+  {
+    var sb = new StringBuilder();
+    var plainTextString = PlainTextString.CreateFromText(""test"");
+    sb.Append(plainTextString);
+  }
+}";
+
+      var diagnostic = Verifier.Diagnostic()
+          .WithSpan (10, 5, 10, 31)
+          .WithMessage (
+              "'System.Text.StringBuilder.Append(object?)' should not be used with a 'Remotion.Web.PlainTextString' argument. "
+              + "Call '.ToString(WebStringEncoding.HtmlWithTransformedLineBreaks)' on the Remotion.Web.PlainTextString instance instead.");
+      await Verifier.VerifyAnalyzerAsync (input, diagnostic);
+    }
+
+    [Test]
+    public async Task StringBuilder_WithWebStringToStringCall_Valid ()
+    {
+      const string input = @"
+using System.Text;
+using Remotion.Web;
+public class A
+{
+  public void Test()
+  {
+    var sb = new StringBuilder();
+    var encodedWebString = WebString.CreateFromText(""test"").ToString(WebStringEncoding.HtmlWithTransformedLineBreaks);
+    sb.Append(encodedWebString);
+  }
+}";
+
+      await Verifier.VerifyAnalyzerAsync (input);
+    }
+
+    [Test]
+    public async Task HtmlTextWriter_WithWebString_Diagnostic ()
+    {
+      const string input = @"
+using System.Web.UI;
+using Remotion.Web;
+public class A
+{
+  public void Test(HtmlTextWriter writer)
+  {
+    var webString = WebString.CreateFromText(""test"");
+    writer.Write(webString);
+  }
+}";
+
+      var diagnostic = Verifier.Diagnostic()
+          .WithSpan (9, 5, 9, 28)
+          .WithMessage (
+              "'System.Web.UI.HtmlTextWriter.Write(object)' should not be used with a 'Remotion.Web.WebString' argument. "
+              + "Use 'Remotion.Web.WebString.Write(HtmlTextWriter)' instead.");
+      await Verifier.VerifyAnalyzerAsync (input, diagnostic);
+    }
+
+    [Test]
+    public async Task HtmlTextWriter_WithPlainTextString_Diagnostic ()
+    {
+      const string input = @"
+using System.Web.UI;
+using Remotion.Web;
+public class A
+{
+  public void Test(HtmlTextWriter writer)
+  {
+    var plainTextString = PlainTextString.CreateFromText(""test"");
+    writer.Write(plainTextString);
+  }
+}";
+
+      var diagnostic = Verifier.Diagnostic()
+          .WithSpan (9, 5, 9, 34)
+          .WithMessage (
+              "'System.Web.UI.HtmlTextWriter.Write(object)' should not be used with a 'Remotion.Web.PlainTextString' argument. "
+              + "Use 'Remotion.Web.PlainTextString.Write(HtmlTextWriter)' instead.");
+      await Verifier.VerifyAnalyzerAsync (input, diagnostic);
+    }
+
+    [Test]
+    public async Task HtmlTextWriter_WithWebStringWriteCalled_Valid ()
+    {
+      const string input = @"
+using System.Web.UI;
+using Remotion.Web;
+public class A
+{
+  public void Test(HtmlTextWriter writer)
+  {
+    var webString = WebString.CreateFromText(""test"");
+    webString.Write(writer);
+  }
+}";
+
+      await Verifier.VerifyAnalyzerAsync (input);
+    }
+
+    [Test]
+    public async Task StringJoin_WithWebStringParams ()
+    {
+      const string input = @"
+using Remotion.Web;
+public class A
+{
+  public string Test()
+  {
+    var webString1 = WebString.CreateFromText(""test1"");
+    return string.Join("", "", webString1, WebString.CreateFromText(""test2""));
+  }
+}";
+
+      var diagnostic = Verifier.Diagnostic()
+          .WithSpan (8, 12, 8, 76)
+          .WithMessage (
+              "'string.Join(string?, params object?[])' should not be used with a 'Remotion.Web.WebString' argument. "
+              + "Encode the Remotion.Web.WebString instances first.");
+      await Verifier.VerifyAnalyzerAsync (input, diagnostic);
+    }
+
+    [Test]
+    public async Task StringJoin_WithWebStringArray ()
+    {
+      const string input = @"
+using Remotion.Web;
+public class A
+{
+  public string Test()
+  {
+    var webStrings = new WebString[]{WebString.CreateFromText(""test1""), WebString.CreateFromText(""test2"")};
+    return string.Join("", "", webStrings);
+  }
+}";
+
+      var diagnostic = Verifier.Diagnostic()
+          .WithSpan (8, 12, 8, 41)
+          .WithMessage (
+              "'string.Join<T>(string?, System.Collections.Generic.IEnumerable<T>)' should not be used with a 'Remotion.Web.WebString[]' argument. "
+              + "Encode the Remotion.Web.WebString[] instances first.");
+      await Verifier.VerifyAnalyzerAsync (input, diagnostic);
+    }
+
+    [Test]
+    public async Task StringJoin_WithPlainTextStringParams ()
+    {
+      const string input = @"
+using Remotion.Web;
+public class A
+{
+  public string Test()
+  {
+    var plainTextString2 = PlainTextString.CreateFromText(""test2"");
+    return string.Join("", "", PlainTextString.CreateFromText(""test1""), plainTextString2);
+  }
+}";
+
+      var diagnostic = Verifier.Diagnostic()
+          .WithSpan (8, 12, 8, 88)
+          .WithMessage (
+              "'string.Join(string?, params object?[])' should not be used with a 'Remotion.Web.PlainTextString' argument. "
+              + "Encode the Remotion.Web.PlainTextString instances first.");
+      await Verifier.VerifyAnalyzerAsync (input, diagnostic);
+    }
+
+    [Test]
+    public async Task StringJoin_WithPlainTextStringArray ()
+    {
+      const string input = @"
+using Remotion.Web;
+public class A
+{
+  public string Test()
+  {
+    var plainTextStrings = new PlainTextString[]{PlainTextString.CreateFromText(""test1""), PlainTextString.CreateFromText(""test2"")};
+    return string.Join("", "", plainTextStrings);
+  }
+}";
+
+      var diagnostic = Verifier.Diagnostic()
+          .WithSpan (8, 12, 8, 47)
+          .WithMessage (
+              "'string.Join<T>(string?, System.Collections.Generic.IEnumerable<T>)' should not be used with a 'Remotion.Web.PlainTextString[]' argument. "
+              + "Encode the Remotion.Web.PlainTextString[] instances first.");
+      await Verifier.VerifyAnalyzerAsync (input, diagnostic);
+    }
+  }
+}
---
+++ Remotion/Web/Development.Analyzers/RMWEB0001_WebStringAnalyzer.cs
@@ -0,0 +1,208 @@
+﻿// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// The re-motion Core Framework is free software; you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// re-motion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+//
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace Web.Development.Analyzers
+{
+  [DiagnosticAnalyzer (LanguageNames.CSharp)]
+  public class RMWEB0001_WebStringAnalyzer : DiagnosticAnalyzer
+  {
+    private record SymbolContext (
+        INamedTypeSymbol WebStringTypeSymbol,
+        INamedTypeSymbol PlainTextStringTypeSymbol,
+        INamedTypeSymbol WebStringEnumerable,
+        INamedTypeSymbol PlainTextStringEnumerable,
+        IMethodSymbol StringBuilderAppendMethodSymbol,
+        IMethodSymbol HtmlTextWriterWriteMethodSymbol,
+        IReadOnlyCollection<IMethodSymbol> StringJoinMethodSymbols);
+
+    public static readonly DiagnosticDescriptor DiagnosticDescriptor = new(
+        "RMWEB0001",
+        "Wrong WebString or PlainTextString usage",
+        "'{0}' should not be used with a '{1}' argument. {2}",
+        "Usage",
+        DiagnosticSeverity.Error,
+        true);
+
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics => ImmutableArray.Create (DiagnosticDescriptor);
+
+    public override void Initialize (AnalysisContext context)
+    {
+      context.EnableConcurrentExecution();
+      context.ConfigureGeneratedCodeAnalysis (GeneratedCodeAnalysisFlags.Analyze);
+
+      context.RegisterCompilationStartAction (
+          compilationContext =>
+          {
+            var webStringTypeSymbol = compilationContext.Compilation.GetTypeByMetadataName ("Remotion.Web.WebString");
+            var plainTextStringTypeSymbol = compilationContext.Compilation.GetTypeByMetadataName ("Remotion.Web.PlainTextString");
+            var stringBuilderTypeSymbol = compilationContext.Compilation.GetTypeByMetadataName ("System.Text.StringBuilder");
+            var htmlTextWriterSymbol = compilationContext.Compilation.GetTypeByMetadataName ("System.Web.UI.HtmlTextWriter");
+
+            if (webStringTypeSymbol == null
+                || plainTextStringTypeSymbol == null
+                || stringBuilderTypeSymbol == null
+                || htmlTextWriterSymbol == null)
+            {
+              return;
+            }
+
+            var objectSymbol = compilationContext.Compilation.GetTypeByMetadataName ("System.Object");
+            var stringSymbol = compilationContext.Compilation.GetTypeByMetadataName ("System.String");
+            var iEnumerable = compilationContext.Compilation.GetTypeByMetadataName ("System.Collections.Generic.IEnumerable`1")!;
+            var webStringEnumerable = iEnumerable.Construct (webStringTypeSymbol);
+            var plainTextStringEnumerable = iEnumerable.Construct (plainTextStringTypeSymbol);
+            var stringBuilderAppendMethodSymbol = GetStringBuilderAppendObjectMethodSymbol (stringBuilderTypeSymbol, objectSymbol);
+            var htmlTextWriterWriteMethodSymbol = GetHtmlTextWriterWriteObjectMethodSymbol (htmlTextWriterSymbol, objectSymbol);
+            var stringJoinMethodSymbols = GetStringJoinMethodSymbols (stringSymbol!);
+
+            var symbolContext = new SymbolContext (
+                webStringTypeSymbol,
+                plainTextStringTypeSymbol,
+                webStringEnumerable,
+                plainTextStringEnumerable,
+                stringBuilderAppendMethodSymbol,
+                htmlTextWriterWriteMethodSymbol,
+                stringJoinMethodSymbols);
+
+            compilationContext.RegisterSyntaxNodeAction (
+                ctx => AnalyzeInvocationExpression (ctx, symbolContext),
+                ImmutableArray.Create (SyntaxKind.InvocationExpression));
+          });
+    }
+
+    private void AnalyzeInvocationExpression (SyntaxNodeAnalysisContext ctx, SymbolContext symbols)
+    {
+      var invocationSymbolInfo = ctx.SemanticModel.GetSymbolInfo (ctx.Node);
+
+      if (invocationSymbolInfo.Symbol == null)
+        return;
+
+      var node = (InvocationExpressionSyntax) ctx.Node;
+      var invocationOriginalDefinitionSymbol = invocationSymbolInfo.Symbol.OriginalDefinition;
+
+      // Check if the invoked method is StringBuilder.Append(object?)
+      if (SymbolEqualityComparer.Default.Equals (invocationOriginalDefinitionSymbol, symbols.StringBuilderAppendMethodSymbol))
+      {
+        var argumentType = GetFirstArgumentTypeSymbol (node, ctx.SemanticModel);
+        // Check if the first argument is WebString or PlainTextString
+        if (SymbolEqualityComparer.Default.Equals (argumentType, symbols.WebStringTypeSymbol)
+            || SymbolEqualityComparer.Default.Equals (argumentType, symbols.PlainTextStringTypeSymbol))
+        {
+          var diagnostic = Diagnostic.Create (
+              DiagnosticDescriptor,
+              ctx.Node.GetLocation(),
+              symbols.StringBuilderAppendMethodSymbol.ToString(),
+              argumentType.ToString(),
+              $"Call '.ToString(WebStringEncoding.HtmlWithTransformedLineBreaks)' on the {argumentType.ToString()} instance instead.");
+          ctx.ReportDiagnostic (diagnostic);
+        }
+      }
+      // Check if the invoked method is HtmlTextWriter.Write(object?)
+      else if (SymbolEqualityComparer.Default.Equals (invocationOriginalDefinitionSymbol, symbols.HtmlTextWriterWriteMethodSymbol))
+      {
+        var argumentType = GetFirstArgumentTypeSymbol (node, ctx.SemanticModel);
+        // Check if the first argument is WebString or PlainTextString
+        if (SymbolEqualityComparer.Default.Equals (argumentType, symbols.WebStringTypeSymbol)
+            || SymbolEqualityComparer.Default.Equals (argumentType, symbols.PlainTextStringTypeSymbol))
+        {
+          var diagnostic = Diagnostic.Create (
+              DiagnosticDescriptor,
+              ctx.Node.GetLocation(),
+              symbols.HtmlTextWriterWriteMethodSymbol.ToString(),
+              argumentType.ToString(),
+              $"Use '{argumentType.ToString()}.Write(HtmlTextWriter)' instead.");
+          ctx.ReportDiagnostic (diagnostic);
+        }
+      }
+      // Check if the invoked method is a string.Join(...) overload
+      else if (TryFindItem (
+          symbols.StringJoinMethodSymbols,
+          s => SymbolEqualityComparer.Default.Equals (invocationOriginalDefinitionSymbol, s),
+          out var methodSymbol))
+      {
+        var argumentTypes = GetArgumentTypeSymbols (node, ctx.SemanticModel);
+        // Check if one of the arguments is a WebString or PlainTextString or implicitly convertible to one of them.
+        if (TryFindItem (
+            argumentTypes,
+            a => SymbolEqualityComparer.Default.Equals (a, symbols.WebStringTypeSymbol)
+                 || SymbolEqualityComparer.Default.Equals (a, symbols.PlainTextStringTypeSymbol)
+                 || ctx.Compilation.ClassifyConversion (a, symbols.WebStringEnumerable).IsImplicit
+                 || ctx.Compilation.ClassifyConversion (a, symbols.PlainTextStringEnumerable).IsImplicit,
+            out var result))
+        {
+          var diagnostic = Diagnostic.Create (
+              DiagnosticDescriptor,
+              ctx.Node.GetLocation(),
+              methodSymbol.ToString(),
+              result.ToString(),
+              $"Encode the {result.ToString()} instances first.");
+          ctx.ReportDiagnostic (diagnostic);
+        }
+      }
+    }
+
+    private static bool TryFindItem<TSymbol> (IEnumerable<TSymbol> methods, Func<TSymbol, bool> predicate, out TSymbol match)
+        where TSymbol : ISymbol
+    {
+      match = methods.FirstOrDefault (predicate)!;
+      return match != null;
+    }
+
+    private static ITypeSymbol? GetFirstArgumentTypeSymbol (InvocationExpressionSyntax node, SemanticModel model)
+    {
+      var firstArgumentExpression = node.ArgumentList.Arguments.First().Expression;
+      return model.GetTypeInfo (firstArgumentExpression).Type;
+    }
+
+    private static IReadOnlyCollection<ITypeSymbol> GetArgumentTypeSymbols (InvocationExpressionSyntax node, SemanticModel model)
+    {
+      return node.ArgumentList.Arguments.Select (s => model.GetTypeInfo (s.Expression).Type).OfType<ITypeSymbol>().ToList();
+    }
+
+    private static IReadOnlyCollection<IMethodSymbol> GetStringJoinMethodSymbols (INamedTypeSymbol stringSymbol)
+    {
+      return stringSymbol
+          .GetMembers ("Join")
+          .Cast<IMethodSymbol>()
+          .ToList();
+    }
+
+    private static IMethodSymbol GetHtmlTextWriterWriteObjectMethodSymbol (INamedTypeSymbol htmlTextWriterSymbol, INamedTypeSymbol? objectSymbol)
+    {
+      return htmlTextWriterSymbol
+          .GetMembers ("Write")
+          .Cast<IMethodSymbol>()
+          .Single (m => m.Parameters.Length == 1 && SymbolEqualityComparer.Default.Equals (m.Parameters.Single().Type, objectSymbol));
+    }
+
+    private static IMethodSymbol GetStringBuilderAppendObjectMethodSymbol (INamedTypeSymbol stringBuilderTypeSymbol, INamedTypeSymbol? objectSymbol)
+    {
+      return stringBuilderTypeSymbol
+          .GetMembers ("Append")
+          .Cast<IMethodSymbol>()
+          .Single (m => m.Parameters.Length == 1 && SymbolEqualityComparer.Default.Equals (m.Parameters.Single().Type, objectSymbol));
+    }
+  }
+}
