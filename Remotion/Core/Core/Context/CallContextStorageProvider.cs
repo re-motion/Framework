1238136d85b218f8d5b8f29888f23ee152f1a795
Patrick Widauer <patrick.widauer@rubicon.eu>
2021-11-30 09:10:33 +0100

    RM-8238 Restore the old CallContextStorageProvider

--- Remotion/Core/Core/Context/CallContextStorageProvider.cs
+++ Remotion/Core/Core/Context/CallContextStorageProvider.cs
@@ -15,28 +15,52 @@
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
 using System;
+#if NETFRAMEWORK
+using Remotion.ServiceLocation;
+using System.Runtime.Remoting.Messaging;
+using Remotion.Utilities;
+#endif
 
 namespace Remotion.Context
 {
   /// <summary>
   /// Implements <see cref="ISafeContextStorageProvider"/> by storing data in the thread-local <see cref="T:System.Runtime.Remoting.Messaging.CallContext"/>.
   /// </summary>
+#if NETFRAMEWORK
+  [ImplementationFor(typeof(ISafeContextStorageProvider), Lifetime = LifetimeKind.Singleton, Position = 1)]
+#else
   [Obsolete("CallContext is deprecated in .NET Core. Use AsyncLocalStorageProvider instead. (Version 3.0.0-alpha.14)", true)]
+#endif
   public class CallContextStorageProvider : ISafeContextStorageProvider
   {
     public object GetData (string key)
     {
+#if NETFRAMEWORK
+      ArgumentUtility.CheckNotNull("key", key);
+      return CallContext.GetData(key);
+#else
       throw new NotSupportedException("CallContext is deprecated in .NET Core. Use AsyncLocalStorageProvider instead. (Version 3.0.0-alpha.14)");
+#endif
     }
 
     public void SetData (string key, object? value)
     {
+#if NETFRAMEWORK
+      ArgumentUtility.CheckNotNull("key", key);
+      CallContext.SetData(key, value);
+#else
       throw new NotSupportedException("CallContext is deprecated in .NET Core. Use AsyncLocalStorageProvider instead. (Version 3.0.0-alpha.14)");
+#endif
     }
 
     public void FreeData (string key)
     {
+#if NETFRAMEWORK
+      ArgumentUtility.CheckNotNull("key", key);
+      CallContext.FreeNamedDataSlot(key);
+#else
       throw new NotSupportedException("CallContext is deprecated in .NET Core. Use AsyncLocalStorageProvider instead. (Version 3.0.0-alpha.14)");
+#endif
     }
   }
 }
