31ca4d330401a86f9fb4794419d0af62c835d250
Michael Ketting <michael.ketting@rubicon.eu>
2022-01-27 18:10:59 +0100

    RM-8343 Update PackageInfo.ps1 to current solution format and to generate markdown

--- packages/PackageInfo.ps1
+++ packages/PackageInfo.ps1
@@ -5,48 +5,103 @@ $licenses = @{
   "http://www.apache.org/licenses/LICENSE-2.0"=@{
     url=$null
     license="Apache License 2.0"};
-  "http://commonservicelocator.codeplex.com/license"=@{
-    url=$null
-    license="Microsoft Public License"; }
-  "http://opensource.org/licenses/mit-license.php"=@{
-    url=$null
-    license="MIT License"};
-  "DependDB.BuildProcessor"=@{
-    url="---"
-    license="Closed source, RUBICON IT GmbH"};
-  "DependDB.BuildProcessor.NuGetPreProcessor"=@{
-    url="---"
-    license="Closed source, RUBICON IT GmbH"};
-  "SharpZipLib"=@{
-    url="https://github.com/re-motion/IO/tree/develop/license/SharpZipLib-0.86"
-    license="GPL, with exceptions for Non-GPL-licenses"};
-  "http://fluentvalidation.codeplex.com/license"=@{
+  "https://licenses.nuget.org/Apache-2.0"=@{
     url=$null
     license="Apache License 2.0"};
-  "https://raw.githubusercontent.com/JetBrains/ExternalAnnotations/master/LICENSE.md"=@{
-    url=$null
-    license="MIT License"};
-  "http://ironpython.codeplex.com/license"=@{
+  "http://fluentvalidation.codeplex.com/license"=@{
     url=$null
     license="Apache License 2.0"};
   "http://logging.apache.org/log4net/license.html"=@{
     url=$null
     license="Apache License 2.0"};
+  "https://raw.githubusercontent.com/NuGet/NuGet.Client/dev/LICENSE.txt"=@{
+    url=$null
+    license="Apache License 2.0"};
+  "http://ironpython.codeplex.com/license"=@{
+    url=$null
+    license="Apache License 2.0"};
+
+  "http://opensource.org/licenses/mit-license.php"=@{
+    url=$null
+    license="MIT License"};
+  "https://licenses.nuget.org/MIT"=@{
+    url=$null
+    license="MIT License"};
+  "https://raw.githubusercontent.com/JetBrains/ExternalAnnotations/master/LICENSE.md"=@{
+    url=$null
+    license="MIT License"};
+  "https://github.com/DefinitelyTyped/NugetAutomation/blob/master/LICENSE.MIT"=@{
+    url=$null
+    license="MIT License"};
+  "https://github.com/dotnet/corefx/blob/master/LICENSE.TXT"=@{
+    url=$null
+    license="MIT License"};
+  "https://github.com/dotnet/standard/blob/master/LICENSE.TXT"=@{
+    url=$null
+    license="MIT License"};
   "http://msbuildextensionpack.codeplex.com/license"=@{
     url=$null
     license="MIT License"};
+  "http://nunit.org/nuget/nunit3-license.txt"=@{
+    url=$null
+    license="MIT License"};
+  "NUnit"=@{
+    url="https://github.com/nunit/nunit/blob/master/LICENSE.txt"
+    license="MIT License"};
+  "NUnit.ConsoleRunner"=@{
+    url="https://github.com/nunit/nunit-console/blob/main/LICENSE.txt"
+    license="MIT License"};
+  "NUnitTestAdapter"=@{
+    url="https://github.com/nunit/nunit-vs-adapter/blob/master/LICENSE.txt"
+    license="MIT License"};
+
+  "SharpZipLib"=@{
+    url="https://github.com/re-motion/IO/tree/develop/license/SharpZipLib-0.86"
+    license="GPL, with exceptions for Non-GPL-licenses"};
+
   "http://www.microsoft.com/web/webpi/eula/nuget_release_eula.htm"=@{
     url=$null
     license="Microsoft Software License"};
+
+  "https://opensource.org/licenses/MS-PL"=@{
+    url=$null
+    license="Microsoft Public License"};
+  "http://commonservicelocator.codeplex.com/license"=@{
+    url=$null
+    license="Microsoft Public License"; }
   "http://nuget4msbuild.codeplex.com/license"=@{
     url=$null
     license="Microsoft Public License"};
+
   "http://nunit.org/nuget/license.html"=@{
     url=$null
     license="NUnit License"};
+
   "http://opensource.org/licenses/BSD-3-Clause"=@{
     url=$null
     license="3-Clause BSD License"};
+  "https://licenses.nuget.org/BSD-3-Clause"=@{
+    url=$null
+    license="3-Clause BSD License"};
+  "https://raw.githubusercontent.com/moq/moq4/master/License.txt"=@{
+    url=$null
+    license="3-Clause BSD License"};
+
+  "CoreForms.Web"=@{
+    url=$null
+    license="Closed source, RUBICON IT GmbH"};
+  "DependDB.BuildProcessor"=@{
+    url=$null
+    license="Closed source, RUBICON IT GmbH"};
+  "DependDB.BuildProcessor.NuGetPreProcessor"=@{
+    url=$null
+    license="Closed source, RUBICON IT GmbH"};
+  "Microsoft.NET.Test.Sdk"=@{
+    url=$null
+    license="Closed source, Microsoft Corporation"};
+  "Microsoft.TypeScript.MSBuild"=@{
+    url=$null
+    license="Closed source, Microsoft Corporation"};
 }
 
 $global:packageIds = New-Object System.Collections.Generic.List[System.Object]
@@ -85,7 +140,7 @@ function Get-PackageDescription ($projectName, $version)
   {
     if ($item.catalogEntry.version -eq $version)
     {
-      return $item.catalogEntry.description
+      return $item.catalogEntry.description.Replace("`r`n","<br>").Replace("`n","<br>")
     }
   }
   return "---"
@@ -98,10 +153,14 @@ function Get-PackageProjectUrl ($projectName, $version)
   {
     if ($item.catalogEntry.version -eq $version)
     {
-      return $item.catalogEntry.projectUrl
+      $projectUrl = $item.catalogEntry.projectUrl
+      if ($projectUrl -ne '')
+      {
+        return $projectUrl
+      }
     }
   }
-  return "---"
+  return $null
 }
 
 function Get-License-Info ($package)
@@ -111,12 +170,14 @@ function Get-License-Info ($package)
     return $licenses[$package.Id]
   }
 
-  if ($package.LicenseUrl -ne $null)
+  if ($package.LicenseUrl -ne $null -and $licenses[$package.LicenseUrl] -ne $null)
   {
     return $licenses[$package.LicenseUrl]
   }
 
-  return "---"
+  return @{
+    url=$package.LicenseUrl
+    license=$package.LicenseUrl};
 }
 
 function Is-Development-Dependency ($projectName)
@@ -125,22 +186,73 @@ function Is-Development-Dependency ($projectName)
   {
     return $false
   }
-  elseif ($projectName.endswith(".UnitTests") -Or $projectName.endswith(".IntegrationTests") -Or $projectName.endswith(".Build"))
+  elseif ($projectName.endswith(".UnitTests"))
+  {
+    return $true
+  }
+  elseif ($projectName.contains(".UnitTests."))
+  {
+    return $true
+  }
+  elseif ($projectName.endswith(".IntegrationTests"))
+  {
+    return $true
+  }
+  elseif ($projectName.contains(".IntegrationTests."))
+  {
+    return $true
+  }
+  elseif ($projectName.endswith(".PerformanceTests"))
+  {
+    return $true
+  }
+  elseif ($projectName.endswith(".Test"))
+  {
+    return $true
+  }
+  elseif ($projectName.startswith("SharedSource."))
+  {
+    return $true
+  }
+  elseif ($projectName.endswith(".Samples"))
+  {
+    return $true
+  }
+  elseif ($projectName.endswith(".NUnit2"))
+  {
+    return $true
+  }
+  elseif ($projectName.endswith(".RhinoMocks"))
+  {
+    return $true
+  }
+  elseif ($projectName.endswith(".Build"))
+  {
+    return $true
+  }
+  elseif ($projectName -eq "Build")
+  {
+    return $true
+  }
+  elseif ($projectName.endswith(".ClientScript"))
   {
     return $true
   }
   else
   {
-    return $false;
+    return $false
   }
 }
-  
+
 function CreateOutput ($showDevDependencies)
 {
   $result = ""
   $packageLicenseUrl = ""
-  $packages = @(Get-Project -All | ? { $_.ProjectName } | % { Get-Package -ProjectName $_.ProjectName } ) | Where-Object { !$_.Id.StartsWith('Remotion') -And !$global:packageIds.Contains($_.Id) -And $(Is-Development-Dependency($_.ProjectName)) -eq $showDevDependencies} | Sort -Property @{Expression = "Id"} -Unique
-  
+  $packages = @(Get-Project -All `
+    | ? { $_.ProjectName } | % { Get-Package -ProjectName $_.ProjectName } ) `
+    | Where-Object { !$_.Id.StartsWith('Remotion') -And !$global:packageIds.Contains($_.Id) -And $(Is-Development-Dependency($_.ProjectName)) -eq $showDevDependencies} `
+    | Sort -Property @{Expression = "Id"} -Unique
+
   foreach ($package in $packages)
   {  
     $version = [string]$package.Versions.Major + '.' + [string]$package.Versions.Minor + '.' + [string]$package.Versions.Patch
@@ -148,14 +260,17 @@ function CreateOutput ($showDevDependencies)
     {
       $version += '.' + [string]$package.Versions.Revision
     }
-    
+
+    $packageNameAndUrl = $package.Id
+    $packageUrl = Get-PackageProjectUrl -ProjectName $package.Id.ToLower() -Version $version
+    if ($packageUrl -ne $null)
+    {
+      $packageNameAndUrl = '[' + $package.Id + '](' + $packageUrl + ')'
+    }
+
     $licenseInfo = Get-License-Info -Package $package
 
     $packageLicenseUrl = $package.LicenseUrl
-    if ($packageLicenseUrl -eq $null)
-    {
-      $packageLicenseUrl = "---"
-    }
     if ($licenseInfo.url -ne $null)
     {
       $packageLicenseUrl = $licenseInfo.url
@@ -163,140 +278,95 @@ function CreateOutput ($showDevDependencies)
 
     $global:packageIds.Add($package.id)
 
+    $license = $licenseInfo.license
+    if ($packageLicenseUrl -ne $null)
+    {
+      $license = '[' + $license + '](' + $packageLicenseUrl + ')'
+    }
+
+    $packageDescription = Get-PackageDescription -ProjectName $package.Id.ToLower() -Version $version
+
+    $result += '| ' + $packageNameAndUrl + ' | ' + $package.Version + ' | ' + $license + ' | ' + $packageDescription + ' |'
     $result += "
-$($package.Id)
-ProjectUrl: $(Get-PackageProjectUrl -ProjectName $package.Id.ToLower() -Version $version)
-License: $($licenseInfo.license)
-LicenseUrl: $($packageLicenseUrl)
-Description: $(Get-PackageDescription -ProjectName $package.Id.ToLower() -Version $version)
 "
   }
-  
-  return $result
-} 
 
-$output = "Distributed Dependencies:
+  return $result
+}
+
+$output = 
+"Distributed Dependencies
+========================
+
+| Name | Version | License | Description |
+|------|---------|---------|-------------|
 "
 
 $output += CreateOutput($false)
 
-$output +=
-"
-Addendum (Distributed Dependencies):
-
-"
-
-$output +=
-"Remotion and Remotion.* (addendum for license)
-This software embeds code based on 'JetBrains Annotations'.
-Copyright (c) 2016 JetBrains http://www.jetbrains.com
-Original software available via ReSharper -> Options -> Code Annotations -> Copy to Clipboard.
-License: MIT License
-LicenseUrl: https://raw.githubusercontent.com/JetBrains/ExternalAnnotations/master/LICENSE.md
-
+$output += "| JetBrains Annotations (Source Code) "
+$output += "| 2017.2 "
+$output += "| [MIT License](https://raw.githubusercontent.com/JetBrains/ExternalAnnotations/master/LICENSE.md) "
+$output += "| This software (assemblies 'Remotion' and 'Remotion.*') embeds code based on 'JetBrains Annotations'. <br> Copyright (c) 2016 JetBrains http://www.jetbrains.com <br> Original software available via ReSharper -> Options -> Code Annotations -> Copy to Clipboard. "
+$output += "|
 "
 
-$output +=
-"Remotion.TypePipe (addendum for license)
-This software embeds code based on 'Microsoft Dynamic Language Runtime'.
-Copyright (c) Microsoft Corporation
-ProjectUrl: https://dlr.codeplex.com
-License: Apache License 2.0
-LicenseUrl: https://dlr.codeplex.com/license
-Relevant code (assembly 'Remotion.TypePipe.dll', namespace 'Remotion.TypePipe.Dlr') contains a copy of '/DLR_Main/Runtime/Microsoft.Scripting.Core', changeset #54115
-
+$output += "| [Microsoft Dynamic Language Runtime](https://github.com/IronLanguages/main) (Source Code) "
+$output += "| dated: 2012-02-27 "
+$output += "| [Apache License 2.0](https://dlr.codeplex.com/license) "
+$output += "| This software (assembly 'Remotion.TypePipe.dll', namespace 'Remotion.TypePipe.Dlr') embeds code from 'Microsoft Dynamic Language Runtime', now a part of 'IronPython'. <br> "
+$output += "Copyright (c) Microsoft Corporation <br> Original software available from 'https://github.com/IronLanguages/main' or 'https://github.com/IronLanguages/ironpython2'. <br> "
+$output += "Relevant code (/Core/Dlr in commit '3dfd952e94c66cdd7f2554602afc463053c1d146' from 2012-02-27) contains a copy of <br> "
+$output += "* 'https://github.com/IronLanguages/main/tree/5846bd06372449247e62e22a374c3d41baf4b9fa/Runtime/Microsoft.Scripting.Core' or <br> "
+$output += "* 'https://github.com/IronLanguages/ironpython2/tree/5846bd06372449247e62e22a374c3d41baf4b9fa/Runtime/Microsoft.Scripting.Core' <br> "
+$output += "  and forked as <br> * 'https://github.com/re-motion/IronLanguages-main/tree/TypePipe-Source/Runtime/Microsoft.Scripting.Core' or <br> "
+$output += "* 'https://github.com/re-motion/IronLanguages-ironpython2/tree/TypePipe-Source/Runtime/Microsoft.Scripting.Core', <br> "
+$output += "originally hosted on 'https://dlr.codeplex.com', now archived as 'https://archive.codeplex.com/?p=dlr'. "
+$output += "|
 "
 
-$output +=
-"Remotion.ObjectBinding.Web (addendum for license)
-This software embeds code based on 'Autocomplete - jQuery plugin'.
-Copyright (c) 2007 Dylan Verheul, Dan G. Switzer, Anjesh Tuladhar, Jörn Zaefferer
-ProjectUrl: http://bassistance.de/jquery-plugins/jquery-plugin-autocomplete/
-License: MIT License
-LicenseUrl: http://www.opensource.org/licenses/mit-license.php
-
+$output += "| [Autocomplete - jQuery plugin](http://bassistance.de/jquery-plugins/jquery-plugin-autocomplete/) (Source Code) "
+$output += "| dated: 2008-07-12 "
+$output += "| [MIT License](http://www.opensource.org/licenses/mit-license.php) "
+$output += "| This software (assembly 'Remotion.ObjectBinding.Web') embeds code based on 'Autocomplete - jQuery plugin'. <br> Copyright (c) 2007 Dylan Verheul, Dan G. Switzer, Anjesh Tuladhar, Jörn Zaefferer <br> http://bassistance.de/jquery-plugins/jquery-plugin-autocomplete/ "
+$output += "|
 "
 
-$output +=
-"Remotion.Web (addendum for license)
-This software embeds a copy of 'jQuery JavaScript Library'.
-Copyright JS Foundation and other contributors, https://js.foundation/
-ProjectUrl: http://jquery.com/
-License: MIT License
-LicenseUrl: https://jquery.org/license
-
+$output += "| [jQuery JavaScript Library](http://jquery.com/) (Source Code) "
+$output += "| 1.6.4 "
+$output += "| [MIT License](https://jquery.org/license) "
+$output += "| This software (assembly 'Remotion.Web') embeds a copy of '[jQuery JavaScript Library](http://jquery.com/)'. <br> Copyright JS Foundation and other contributors, https://js.foundation/ "
+$output += "|
 "
 
-$output +=
-"Remotion.Web (addendum for license)
-This software embeds code based on 'jquery.bgiframe'.
-Copyright (c) 2006 Brandon Aaron (http://brandonaaron.net)
-ProjectUrl: https://github.com/brandonaaron/bgiframe
-License: MIT License
-LicenseUrl: https://raw.githubusercontent.com/brandonaaron/bgiframe/master/LICENSE.txt
+$output += "| [jquery.bgiframe](https://github.com/brandonaaron/bgiframe) (Source Code) "
+$output += "| dated: 2009-07-13 "
+$output += "| [MIT License](https://raw.githubusercontent.com/brandonaaron/bgiframe/master/LICENSE.txt) "
+$output += "| This software (assembly 'Remotion.Web') embeds code based on 'jquery.bgiframe'. <br> Copyright (c) 2006 Brandon Aaron (http://brandonaaron.net) "
+$output += "|
 "
 
 $output += "
-###############################################################################
-
 "
 
 
 $output +=
-"Development Dependencies:
+"Development Dependencies
+=========================
+
+| Name | Version | License | Description |
+|------|---------|---------|-------------|
 "
 
 $output += CreateOutput($true)
 
-$output +=
-"
-Addendum (Development Dependencies):
-
-"
-
-$output +=
-"RhinoMocks
-ProjectUrl: http://hibernatingrhinos.com/oss/rhino-mocks
-License: 2-Clause BSD License
-LicenseUrl: http://opensource.org/licenses/bsd-license.php
-Description: Mocking Framework
-
+$output += "| [SandcastleHelpFileBuilder](https://github.com/EWSoftware/SHFB) "
+$output += "| 1.9.5.0 "
+$output += "| [Microsoft Public License](https://github.com/EWSoftware/SHFB/blob/master/LICENSE) "
+$output += "| This package allows you to deploy the Sandcastle Help File Builder tools inside of a project to build help files without installing the tools manually such as on a build server. Some limitations apply. "
+$output += "|
 "
 
-$output +=
-"MSBuild Community Tasks
-ProjectUrl: https://github.com/loresoft/msbuildtasks/
-License: 2-Clause BSD License
-LicenseUrl: http://opensource.org/licenses/bsd-license.php
-Description: The MSBuild Community Tasks project is a collection of open source tasks for MSBuild.
+$output > re-motion_3rdParty.md
 
-"
-
-$output +=
-"SandcastleHelpFileBuilder
-ProjectUrl: https://github.com/EWSoftware/SHFB
-License: Microsoft Public License
-LicenseUrl: http://shfb.codeplex.com/license
-Description: This package allows you to deploy the Sandcastle Help File Builder tools inside of a project to build help files without installing the tools manually such as on a build server. Some limitations apply.
-
-"
-
-$output +=
-"Selenium Core
-ProjectUrl: http://www.seleniumhq.org/projects/ide/
-License: Apache License 2.0
-LicenseUrl: https://github.com/SeleniumHQ/selenium/blob/master/LICENSE
-Description: Javascript-based testrunner web clients
-
-"
-
-$output +=
-"Subversion Client
-ProjectUrl: https://subversion.apache.org/
-License: Apache License 2.0
-LicenseUrl: http://www.apache.org/licenses/LICENSE-2.0
-Description: Source Control Client"
-
-$output > re-motion_1.20_3rdParty.txt
-
-write-host Written to re-motion_1.20_3rdParty.txt
+write-host Written to re-motion_3rdParty.md
