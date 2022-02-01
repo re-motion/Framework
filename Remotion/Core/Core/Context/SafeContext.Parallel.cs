fa88c9bf8050fc443330aeade3cb550bca8b0545
Patrick Widauer <patrick.widauer@rubicon.eu>
2022-02-01 09:35:46 +0100

    RM-8330 Create helper constructs for SafeContext aware Parallel scenarios

---
+++ Remotion/Core/Core/Context/SafeContext.Parallel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Remotion.Context
+{
+  public static partial class SafeContext
+  {
+    /// <summary>
+    /// Provides methods for executing <see cref="System.Threading.Tasks.Parallel"/> actions which are <see cref="SafeContext"/> aware.
+    /// <see cref="SafeContext"/> values do not flow into the executed Parallel action.
+    /// </summary>
+    public static class Parallel
+    {
+      /// <summary>
+      /// Opens a <see cref="SafeContextBoundary"/> in which <see cref="System.Threading.Tasks.Parallel"/> actions can be taken without
+      /// flowing <see cref="SafeContext"/> values into the callbacks.
+      /// </summary>
+      /// <example>
+      /// <code>
+      /// using (SafeContext.Parallel.OpenSafeContextBoundary())
+      /// {
+      ///   // SafeContext values no longer available
+      ///   Parallel.For(0, 10, v => ...)
+      /// }
+      /// // SafeContext values available again
+      /// </code>
+      /// </example>
+      [MustUseReturnValue]
+      public static SafeContextBoundary OpenSafeContextBoundary () => Instance.OpenSafeContextBoundary();
+
+      /// <summary>
+      /// Opens a <see cref="SafeContextBoundary"/> in which <see cref="System.Threading.Tasks.Parallel"/> actions can be taken without
+      /// flowing <see cref="SafeContext"/> values into the callbacks.
+      /// </summary>
+      /// <example>
+      /// <code>
+      /// using (SafeContext.Parallel.OpenSafeContextBoundary())
+      /// {
+      ///   // SafeContext values no longer available
+      ///   Parallel.For(0, 10, v => ...)
+      /// }
+      /// // SafeContext values available again
+      /// </code>
+      /// </example>
+      [MustUseReturnValue]
+      public static SafeContextBoundary OpenSafeContextBoundary (ISafeContextStorageProvider provider) => provider.OpenSafeContextBoundary();
+    }
+  }
+}
---
+++ Remotion/Core/UnitTests/Context/SafeContextTest.Parallel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Moq;
+using NUnit.Framework;
+using Remotion.Context;
+
+namespace Remotion.UnitTests.Context
+{
+  public partial class SafeContextTest
+  {
+    [TestFixture]
+    public class ParallelTest
+    {
+      private Mock<ISafeContextStorageProvider> _safeContextProviderMock;
+
+      [SetUp]
+      public void SetUp ()
+      {
+        ResetSafeContextStorageProvider();
+
+        _safeContextProviderMock = new Mock<ISafeContextStorageProvider>(MockBehavior.Strict);
+        _safeContextProviderMock.Setup(e => e.OpenSafeContextBoundary()).Returns((SafeContextBoundary)default);
+      }
+
+      [TearDown]
+      public void TearDown ()
+      {
+        ResetSafeContextStorageProvider();
+      }
+
+      [Test]
+      public void Run_WithImplicitSafeContextStorageProvider_OpensSafeContextBoundary ()
+      {
+        var safeContextStorageProvider = _safeContextProviderMock.Object;
+        using (SetupImplicitSafeContextStorageProvider(safeContextStorageProvider))
+        {
+          using (SafeContext.Parallel.OpenSafeContextBoundary())
+            _safeContextProviderMock.Verify(e => e.OpenSafeContextBoundary());
+        }
+      }
+
+      [Test]
+      public void Run_WithExplicitSafeContextStorageProvider_OpensSafeContextBoundary ()
+      {
+        var safeContextStorageProvider = _safeContextProviderMock.Object;
+        using (SafeContext.Parallel.OpenSafeContextBoundary(safeContextStorageProvider))
+          _safeContextProviderMock.Verify(e => e.OpenSafeContextBoundary());
+      }
+    }
+  }
+}
