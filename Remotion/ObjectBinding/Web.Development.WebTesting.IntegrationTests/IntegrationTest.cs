f589b9c2521e9efdcecd3b2e09df9097cf7cd52f
Michael Ketting <michael.ketting@rubicon.eu>
2023-02-16 16:13:25 +0100

    RM-8744 Remove obsolete hostname resolution for MS Edge-workaround
    MS Edge v98 no longer shows the performance problem when trying to resolve the hostname. Therefor, the workaround can be removed.

--- Remotion/ObjectBinding/Web.Development.WebTesting.IntegrationTests/IntegrationTest.cs
+++ Remotion/ObjectBinding/Web.Development.WebTesting.IntegrationTests/IntegrationTest.cs
@@ -21,8 +21,6 @@ using NUnit.Framework.Interfaces;
 using Remotion.Web.Development.WebTesting;
 using Remotion.Web.Development.WebTesting.ExecutionEngine.PageObjects;
 using Remotion.Web.Development.WebTesting.IntegrationTests.Infrastructure;
-using Remotion.Web.Development.WebTesting.Utilities;
-using Remotion.Web.Development.WebTesting.WebDriver;
 
 namespace Remotion.ObjectBinding.Web.Development.WebTesting.IntegrationTests
 {
@@ -31,8 +29,6 @@ namespace Remotion.ObjectBinding.Web.Development.WebTesting.IntegrationTests
   /// </summary>
   public abstract class IntegrationTest
   {
-    private static Lazy<Uri> s_webApplicationRoot;
-
     private WebTestHelper _webTestHelper;
 
     protected virtual bool MaximizeMainBrowserSession
@@ -56,17 +52,6 @@ namespace Remotion.ObjectBinding.Web.Development.WebTesting.IntegrationTests
       _webTestHelper = WebTestHelper.CreateFromConfiguration<CustomWebTestConfigurationFactory>();
 
       _webTestHelper.OnFixtureSetUp(MaximizeMainBrowserSession);
-      s_webApplicationRoot = new Lazy<Uri>(
-          () =>
-          {
-            var uri = new Uri(_webTestHelper.TestInfrastructureConfiguration.WebApplicationRoot);
-
-            // RM-7401: Edge loads pages slower due to repeated hostname resolution.
-            if (_webTestHelper.BrowserConfiguration.IsEdge())
-              return HostnameResolveHelper.ResolveHostname(uri);
-
-            return uri;
-          });
     }
 
     [SetUp]
@@ -92,7 +77,7 @@ namespace Remotion.ObjectBinding.Web.Development.WebTesting.IntegrationTests
     {
       var userControlUrl = string.Format("Controls/{0}UserControl.ascx", userControl);
 
-      var url = string.Format("{0}ControlTest.wxe?UserControl={1}", s_webApplicationRoot.Value, userControlUrl);
+      var url = string.Format("{0}ControlTest.wxe?UserControl={1}", _webTestHelper.TestInfrastructureConfiguration.WebApplicationRoot, userControlUrl);
       _webTestHelper.MainBrowserSession.Window.Visit(url);
       _webTestHelper.AcceptPossibleModalDialog();
 
