daa768c93860cf8a4b7ea359a055ec136259b469
Lukas Hartinger <lukas.hartinger@rubicon.eu>
2023-05-11 11:54:18 +0200

    RM-8812 Implement tests for PropertyValidationRule

--- Remotion/Validation/UnitTests/Rules/ObjectValidationRuleTest.cs
+++ Remotion/Validation/UnitTests/Rules/ObjectValidationRuleTest.cs
@@ -1,4 +1,10 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using Moq;
+using NUnit.Framework;
+using Remotion.Validation.Results;
+using Remotion.Validation.Rules;
+using Remotion.Validation.Validators;
 
 namespace Remotion.Validation.UnitTests.Rules
 {
@@ -6,16 +12,117 @@ namespace Remotion.Validation.UnitTests.Rules
   public class ObjectValidationRuleTest
   {
     [Test]
-    [Ignore("TODO RM-5906")]
-    public void Validate ()
+    public void Validate_ReturnsValidationFailures ()
     {
+      Func<int, bool> condition = i => true;
+      var context = new ValidationContext(1);
+
+      var failure1 = new ObjectValidationFailure(
+          15,
+          "15 is not a number",
+          "Localized validation message: 15 is not a number.");
+
+      var failure2 = new ObjectValidationFailure(
+          42,
+          "42 is not a number",
+          "Localized validation message: 42 is not a number.");
+
+      var validatorMock1 = new Mock<IObjectValidator>();
+      validatorMock1.Setup(_ => _.Validate(It.IsAny<ObjectValidatorContext>()))
+          .Returns(new[] { failure1 })
+          .Verifiable();
+
+      var validatorMock2 = new Mock<IObjectValidator>();
+      validatorMock2.Setup(_ => _.Validate(It.IsAny<ObjectValidatorContext>()))
+          .Returns(new[] { failure2 })
+          .Verifiable();
+
+      var rule = new ObjectValidationRule<int>(condition, new[] { validatorMock1.Object, validatorMock2.Object });
+
+      var result = rule.Validate(context);
+      Assert.That(result, Is.EquivalentTo(new[] { failure1, failure2 }));
     }
 
     [Test]
-    [Ignore("TODO RM-5906")]
-    public void IsActive ()
+    public void Validate_TryToValidateNull_ReturnsEmpty ()
     {
-      // consider better name than "IsActive"
+      bool condition (object o)
+      {
+        return o != null;
+      }
+
+      var context = new ValidationContext(null);
+      var rule = new ObjectValidationRule<object>(condition, new List<IObjectValidator>());
+
+      var result = rule.Validate(context);
+
+      Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Validate_WithNullCondition_ReturnsEmpty ()
+    {
+      Func<object, bool> condition = null;
+      var context = new ValidationContext(new object());
+      var rule = new ObjectValidationRule<object>(condition, new List<IObjectValidator>());
+
+      var result = rule.Validate(context);
+
+      Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void Validate_WithUnfulfilledCondition_ReturnsEmpty ()
+    {
+      Func<int, bool> condition = i => i > 0;
+      var context = new ValidationContext(-1);
+      var rule = new ObjectValidationRule<int>(condition, new List<IObjectValidator>());
+
+      var result = rule.Validate(context);
+
+      Assert.That(result, Is.Empty);
+    }
+
+    [Test]
+    public void IsActive_TryToValidateNull_ReturnsFalse ()
+    {
+      Func<object, bool> condition = o => true;
+      var context = new ValidationContext(null);
+      var rule = new ObjectValidationRule<object>(condition, new List<IObjectValidator>());
+
+      var result = rule.IsActive(context);
+
+      Assert.That(result, Is.False);
+    }
+
+    [Test]
+    public void IsActive_WithNullCondition_ReturnsTrue ()
+    {
+      Func<object, bool> condition = null;
+
+      var context = new ValidationContext(new object());
+      var rule = new ObjectValidationRule<object>(condition, new List<IObjectValidator>());
+
+      var result = rule.IsActive(context);
+
+      Assert.That(result, Is.True);
+    }
+
+    [Test]
+    public void IsActive_ReturnsValueFromCondition ()
+    {
+      Func<object, bool> alwaysTrueCondition = o => true;
+      Func<object, bool> alwaysFalseCondition = o => false;
+
+      var context = new ValidationContext(new object());
+      var trueRule = new ObjectValidationRule<object>(alwaysTrueCondition, new List<IObjectValidator>());
+      var falseRule = new ObjectValidationRule<object>(alwaysFalseCondition, new List<IObjectValidator>());
+
+      var resultTrueCondition = trueRule.IsActive(context);
+      var resultFalseCondition = falseRule.IsActive(context);
+
+      Assert.That(resultTrueCondition, Is.True);
+      Assert.That(resultFalseCondition, Is.False);
     }
   }
 }
