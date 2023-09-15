2ca5bb5972c1980808fba18e0959f1e2f710bb55
Michael Ketting <michael.ketting@rubicon.eu>
2023-09-15 15:31:51 +0200

    RM-8960 Apply correct MaxLength to rendered TextBox.

--- Remotion/ObjectBinding/Web/UI/Controls/Styles.cs
+++ Remotion/ObjectBinding/Web/UI/Controls/Styles.cs
@@ -313,8 +313,9 @@ namespace Remotion.ObjectBinding.Web.UI.Controls
     {
       textBox.ApplyStyle(this);
 
-      if (_maxLength != null && _checkClientSideMaxLength != false)
-        textBox.MaxLength = _maxLength.Value;
+      var maxLength = GetMaxLength();
+      if (maxLength != null && _checkClientSideMaxLength != false)
+        textBox.MaxLength = maxLength.Value;
 
       if (_columns != null)
         textBox.Columns = _columns.Value;
