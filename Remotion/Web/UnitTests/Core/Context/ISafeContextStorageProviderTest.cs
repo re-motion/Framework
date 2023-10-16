42b1eb71a7b5a8091ae5aa66405358fac4ae869e
Michael Ketting <michael.ketting@rubicon.eu>
2023-10-16 21:23:48 +0200

    RM-8972 Remove HttpContextStorageProviderWithAsyncLocalBasedFallback from IoC registration

--- Remotion/Web/UnitTests/Core/Context/ISafeContextStorageProviderTest.cs
+++ Remotion/Web/UnitTests/Core/Context/ISafeContextStorageProviderTest.cs
@@ -1,9 +1,10 @@
 ﻿using System;
-using System.Linq;
 using NUnit.Framework;
 using Remotion.Context;
 using Remotion.ServiceLocation;
+#if NETFRAMEWORK
 using Remotion.Web.Context;
+#endif
 
 namespace Remotion.Web.UnitTests.Core.Context
 {
@@ -26,17 +27,17 @@ namespace Remotion.Web.UnitTests.Core.Context
 #if NETFRAMEWORK
       Assert.That(instances, Is.InstanceOf(typeof(HttpContextStorageProviderWithCallContextBasedFallback)));
 #else
-      Assert.That(instances, Is.InstanceOf(typeof(HttpContextStorageProviderWithAsyncLocalBasedFallback)));
+      Assert.That(instances, Is.InstanceOf(typeof(AsyncLocalStorageProvider)));
 #endif
     }
 
     [Test]
-    public void GetInstance_Twice_ReturnsDifferentInstance ()
+    public void GetInstance_Twice_ReturnsSameInstance ()
     {
       var instance1 = _serviceLocator.GetInstance<ISafeContextStorageProvider>();
       var instance2 = _serviceLocator.GetInstance<ISafeContextStorageProvider>();
 
-      Assert.That(instance1, Is.Not.SameAs(instance2));
+      Assert.That(instance1, Is.SameAs(instance2));
     }
   }
 }
