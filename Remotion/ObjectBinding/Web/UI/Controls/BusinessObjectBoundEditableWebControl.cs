2b77d696ba8eeb7349c8946586f8d95dbb117766
Michael Ketting <michael.ketting@rubicon.eu>
2023-11-09 16:14:09 +0100

    RM-8611 Add checks to not evaluate the postback collection if the control was not rendered with active input in the previous page lifecycle

--- Remotion/ObjectBinding/Web/UI/Controls/BusinessObjectBoundEditableWebControl.cs
+++ Remotion/ObjectBinding/Web/UI/Controls/BusinessObjectBoundEditableWebControl.cs
@@ -39,6 +39,9 @@ namespace Remotion.ObjectBinding.Web.UI.Controls
     private bool _isDirty;
     private bool _hasBeenRenderedInPreviousLifecycle;
     private bool _isRenderedInCurrentLifecycle;
+    private bool _hasBeenReadOnlyInPreviousLifecycle;
+    private bool _hasBeenEnabledInPreviousLifecycle;
+    private bool _hasBeenVisibleInPreviousLifecycle;
 
     /// <summary>
     /// Overrides the base method to call <see cref="ISmartPage.RegisterControlForClientSideDirtyStateTracking"/>
@@ -322,17 +325,29 @@ namespace Remotion.ObjectBinding.Web.UI.Controls
     /// Gets a value that determines whether a server control needs to load data from the posted form values
     /// to its internal state.
     /// </summary>
-    /// <value><see langword="true"/> if the control has been rendered in the previous lifecycle,
+    /// <value><see langword="true"/> if the control has been rendered as a visible, enabled, and editable element in the previous lifecycle,
     /// or if it is on a <see cref="IWxePage"/> and <see cref="IWxePage.IsOutOfSequencePostBack"/> is <see langword="true"/>.</value>
     protected virtual bool RequiresLoadPostData
     {
       get
       {
         IWxePage wxePage = Page as IWxePage;
-        if (wxePage != null)
-          return _hasBeenRenderedInPreviousLifecycle || wxePage.IsOutOfSequencePostBack;
+        if (wxePage != null && wxePage.IsOutOfSequencePostBack == true)
+          return true;
 
-        return _hasBeenRenderedInPreviousLifecycle;
+        if (_hasBeenReadOnlyInPreviousLifecycle)
+          return false;
+
+        if (!_hasBeenEnabledInPreviousLifecycle)
+          return false;
+
+        if (!_hasBeenVisibleInPreviousLifecycle)
+          return false;
+
+        if (!_hasBeenRenderedInPreviousLifecycle)
+          return false;
+
+        return true;
       }
     }
 
@@ -347,6 +362,9 @@ namespace Remotion.ObjectBinding.Web.UI.Controls
       base.LoadControlState (values[0]);
       _isDirty = (bool) values[1];
       _hasBeenRenderedInPreviousLifecycle = (bool) values[2];
+      _hasBeenReadOnlyInPreviousLifecycle = (bool) values[3];
+      _hasBeenEnabledInPreviousLifecycle = (bool) values[4];
+      _hasBeenVisibleInPreviousLifecycle = (bool) values[5];
     }
 
     /// <summary>
@@ -356,10 +374,13 @@ namespace Remotion.ObjectBinding.Web.UI.Controls
     /// <returns>An object containing the state required to be loaded in the next lifecycle.</returns>
     protected override object SaveControlState ()
     {
-      object[] values = new object[3];
+      object[] values = new object[6];
       values[0] = base.SaveControlState();
       values[1] = _isDirty;
       values[2] = _isRenderedInCurrentLifecycle;
+      values[3] = IsReadOnly;
+      values[4] = Enabled;
+      values[5] = Visible;
       return values;
     }
 
