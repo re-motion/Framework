a6465faefeac33b5733e6b9217d451b427076dcd
Joel Kudiyirickal <joel.kudiyirickal@rubicon.eu>
2023-11-21 15:29:54 +0100

    Add Adapter class

---
+++ Remotion/Web/Development.WebTesting/Configuration/WebTestConfigurationAdapter.cs
@@ -0,0 +1,57 @@
+﻿// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// The re-motion Core Framework is free software; you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// re-motion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+//
+using System;
+
+namespace Remotion.Web.Development.WebTesting.Configuration
+{
+  public class WebTestConfigurationAdapter : IWebTestConfiguration
+  {
+    public string Browser => WebTestConfiguration.Current.Browser;
+
+    public TimeSpan SearchTimeout => WebTestConfigurationSection.Current.SearchTimeout;
+
+    public TimeSpan DownloadStartedTimeout => WebTestConfigurationSection.Current.DownloadStartedTimeout;
+
+    public TimeSpan DownloadUpdatedTimeout => WebTestConfigurationSection.Current.DownloadUpdatedTimeout;
+
+    public TimeSpan RetryInterval => WebTestConfigurationSection.Current.RetryInterval;
+
+    public TimeSpan AsyncJavaScriptTimeout => WebTestConfigurationSection.Current.AsyncJavaScriptTimeout;
+
+    public bool Headless => WebTestConfigurationSection.Current.Headless;
+
+    public string WebApplicationRoot => WebTestConfigurationSection.Current.WebApplicationRoot;
+
+    public string ScreenshotDirectory => WebTestConfigurationSection.Current.ScreenshotDirectory;
+
+    public string LogsDirectory => WebTestConfigurationSection.Current.LogsDirectory;
+
+    public bool CloseBrowserWindowsOnSetUpAndTearDown => WebTestConfigurationSection.Current.CloseBrowserWindowsOnSetUpAndTearDown;
+
+    public bool CleanUpUnmatchedDownloadedFiles => WebTestConfigurationSection.Current.CleanUpUnmatchedDownloadedFiles;
+
+    public string RequestErrorDetectionStrategy => WebTestConfigurationSection.Current.RequestErrorDetectionStrategyTypeName;
+
+    public IChromiumConfiguration Chrome { get; } = new ChromeConfigurationAdapter();
+
+    public IChromiumConfiguration Edge { get; } = new EdgeConfigurationAdapter();
+
+    public IHosting Hosting { get; } = new HostingAdapter();
+
+    public ITestSiteLayout TestSiteLayout { get; } = new TestSiteLayoutAdapter();
+  }
+}
