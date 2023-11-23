cf6f5276c011b243884094ee88e5da1c2b0e291c
Joel Kudiyirickal <joel.kudiyirickal@rubicon.eu>
2023-11-23 15:13:07 +0100

    RM-8550 Add NonceGenerator

---
+++ Remotion/Web/Core/ContentSecurityPolicy/NonceGenerator.cs
@@ -0,0 +1,39 @@
+﻿// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// The re-motion Core Framework is free software; you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// re-motion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+using System;
+using System.Security.Cryptography;
+
+namespace Remotion.Web.ContentSecurityPolicy
+{
+  /// <summary>
+  /// Implements <see cref="INonceGenerator"/> to generate alpha numeric nonce values.
+  /// </summary>
+  /// <threadsafety static="true" instance="true" />
+  public class NonceGenerator : INonceGenerator
+  {
+    public NonceGenerator ()
+    {
+    }
+
+    public string GenerateAlphaNumericNonce ()
+    {
+      Span<byte> data = stackalloc byte[8];
+      RandomNumberGenerator.Fill(data);
+
+      return Convert.ToHexString(data);
+    }
+  }
+}
