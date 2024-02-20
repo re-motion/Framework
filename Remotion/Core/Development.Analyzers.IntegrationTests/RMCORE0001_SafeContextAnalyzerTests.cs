a95e713309802429d58828d0ba42889260981eaa
Lukas Hartinger <lukas.hartinger@rubicon.eu>
2024-02-20 10:16:44 +0100

    RM-9087 Improve safe context analyzer to detect correct usages

--- Remotion/Core/Development.Analyzers.IntegrationTests/RMCORE0001_SafeContextAnalyzerTests.cs
+++ Remotion/Core/Development.Analyzers.IntegrationTests/RMCORE0001_SafeContextAnalyzerTests.cs
@@ -164,6 +164,355 @@ public class A
     }
 
     [Test]
+    public async Task SafeContext_WithTasksParallelAndSafeContextBoundary_NoDiagnostic ()
+    {
+      const string input = @"
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using Remotion.Context;
+public class A
+{
+  public void Test ()
+  {
+    using var boundary = SafeContext.Parallel.OpenSafeContextBoundary();
+
+    SafeContext.Instance.SetData(""a"", 3);
+
+    Parallel.For(0, 5, i => { i = (int) SafeContext.Instance.GetData(""a"");});
+
+    Parallel.ForEach(Enumerable.Range(5, 10).ToList(), i => Console.Write(i));
+
+    Parallel.ForEachAsync(
+        Enumerable.Range(0, 5),
+        new ParallelOptions(), 
+        (i, token) => ValueTask.CompletedTask
+    );
+
+    Parallel.Invoke(() => Console.Write(""a""));
+  }
+}
+";
+      await Verifier.VerifyAnalyzerAsync(input, true);
+    }
+
+    [Test]
+    public async Task SafeContext_WithTasksParallelAndSafeContextBoundaryFromInstance_NoDiagnostic ()
+    {
+      const string input = @"
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using Remotion.Context;
+public class A
+{
+  public void Test ()
+  {
+    using var boundary = SafeContext.Instance.OpenSafeContextBoundary();
+
+    SafeContext.Instance.SetData(""a"", 3);
+
+    Parallel.For(0, 5, i => { i = (int) SafeContext.Instance.GetData(""a"");});
+
+    Parallel.ForEach(Enumerable.Range(5, 10).ToList(), i => Console.Write(i));
+
+    Parallel.ForEachAsync(
+        Enumerable.Range(0, 5),
+        new ParallelOptions(), 
+        (i, token) => ValueTask.CompletedTask
+    );
+
+    Parallel.Invoke(() => Console.Write(""a""));
+  }
+}
+";
+      await Verifier.VerifyAnalyzerAsync(input, true);
+    }
+
+    [Test]
+    public async Task SafeContext_WithTasksParallelAndSafeContextBoundaryAfterUsages_Diagnostic ()
+    {
+      const string input = @"
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using Remotion.Context;
+public class A
+{
+  public void Test ()
+  {
+    SafeContext.Instance.SetData(""a"", 3);
+
+    Parallel.For(0, 5, i => { i = (int) SafeContext.Instance.GetData(""a"");});
+
+    Parallel.ForEach(Enumerable.Range(5, 10).ToList(), i => Console.Write(i));
+
+    Parallel.ForEachAsync(
+        Enumerable.Range(0, 5),
+        new ParallelOptions(), 
+        (i, token) => ValueTask.CompletedTask
+    );
+
+    Parallel.Invoke(() => Console.Write(""a""));
+
+    using var boundary = SafeContext.Parallel.OpenSafeContextBoundary();
+  }
+}
+";
+      var diagnosticFor = Verifier.Diagnostic(Analyzer.AlternativeDescriptor)
+          .WithSpan(12, 5, 12, 77)
+          .WithMessage(
+              "'System.Threading.Tasks.Parallel.For' should not be used with SafeContext by itself, use 'Remotion.Context.SafeContext.Parallel.OpenSafeContextBoundary()' to create a safe context boundary.");
+
+      var diagnosticForEach = Verifier.Diagnostic(Analyzer.AlternativeDescriptor)
+          .WithSpan(14, 5, 14, 78)
+          .WithMessage(
+              "'System.Threading.Tasks.Parallel.ForEach' should not be used with SafeContext by itself, use 'Remotion.Context.SafeContext.Parallel.OpenSafeContextBoundary()' to create a safe context boundary.");
+
+      var diagnosticForEachAsync = Verifier.Diagnostic(Analyzer.AlternativeDescriptor)
+          .WithSpan(16, 5, 20, 6)
+          .WithMessage(
+              "'System.Threading.Tasks.Parallel.ForEachAsync' should not be used with SafeContext by itself, use 'Remotion.Context.SafeContext.Parallel.OpenSafeContextBoundary()' to create a safe context boundary.");
+
+      var diagnosticInvoke = Verifier.Diagnostic(Analyzer.AlternativeDescriptor)
+          .WithSpan(22, 5, 22, 46)
+          .WithMessage(
+              "'System.Threading.Tasks.Parallel.Invoke' should not be used with SafeContext by itself, use 'Remotion.Context.SafeContext.Parallel.OpenSafeContextBoundary()' to create a safe context boundary.");
+
+      await Verifier.VerifyAnalyzerAsync(input, true, diagnosticFor, diagnosticForEach, diagnosticForEachAsync, diagnosticInvoke);
+    }
+
+    [Test]
+    public async Task SafeContext_WithTasksParallelAndUselessUsingStatement_Diagnostic ()
+    {
+      const string input = @"
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Linq;
+using Remotion.Context;
+public class A
+{
+  public void Test ()
+  {
+    using var reader = File.OpenText(""notAFile"");
+
+    SafeContext.Instance.SetData(""a"", 3);
+
+    Parallel.For(0, 5, i => { i = (int) SafeContext.Instance.GetData(""a"");});
+
+    Parallel.ForEach(Enumerable.Range(5, 10).ToList(), i => Console.Write(i));
+
+    Parallel.ForEachAsync(
+        Enumerable.Range(0, 5),
+        new ParallelOptions(), 
+        (i, token) => ValueTask.CompletedTask
+    );
+
+    Parallel.Invoke(() => Console.Write(""a""));
+  }
+}
+";
+      var diagnosticFor = Verifier.Diagnostic(Analyzer.AlternativeDescriptor)
+          .WithSpan(15, 5, 15, 77)
+          .WithMessage(
+              "'System.Threading.Tasks.Parallel.For' should not be used with SafeContext by itself, use 'Remotion.Context.SafeContext.Parallel.OpenSafeContextBoundary()' to create a safe context boundary.");
+
+      var diagnosticForEach = Verifier.Diagnostic(Analyzer.AlternativeDescriptor)
+          .WithSpan(17, 5, 17, 78)
+          .WithMessage(
+              "'System.Threading.Tasks.Parallel.ForEach' should not be used with SafeContext by itself, use 'Remotion.Context.SafeContext.Parallel.OpenSafeContextBoundary()' to create a safe context boundary.");
+
+      var diagnosticForEachAsync = Verifier.Diagnostic(Analyzer.AlternativeDescriptor)
+          .WithSpan(19, 5, 23, 6)
+          .WithMessage(
+              "'System.Threading.Tasks.Parallel.ForEachAsync' should not be used with SafeContext by itself, use 'Remotion.Context.SafeContext.Parallel.OpenSafeContextBoundary()' to create a safe context boundary.");
+
+      var diagnosticInvoke = Verifier.Diagnostic(Analyzer.AlternativeDescriptor)
+          .WithSpan(25, 5, 25, 46)
+          .WithMessage(
+              "'System.Threading.Tasks.Parallel.Invoke' should not be used with SafeContext by itself, use 'Remotion.Context.SafeContext.Parallel.OpenSafeContextBoundary()' to create a safe context boundary.");
+
+      await Verifier.VerifyAnalyzerAsync(input, true, diagnosticFor, diagnosticForEach, diagnosticForEachAsync, diagnosticInvoke);
+    }
+
+    [Test]
+    public async Task SafeContext_WithTasksParallelAndSeveralLocalUsingDeclarations1_NoDiagnostic ()
+    {
+      const string input = @"
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Linq;
+using Remotion.Context;
+public class A
+{
+  public void Test ()
+  {
+    using var boundary = SafeContext.Parallel.OpenSafeContextBoundary();
+    using var reader = File.OpenText(""notAFile"");
+
+    SafeContext.Instance.SetData(""a"", 3);
+
+    Parallel.For(0, 5, i => { i = (int) SafeContext.Instance.GetData(""a"");});
+
+    Parallel.ForEach(Enumerable.Range(5, 10).ToList(), i => Console.Write(i));
+
+    Parallel.ForEachAsync(
+        Enumerable.Range(0, 5),
+        new ParallelOptions(), 
+        (i, token) => ValueTask.CompletedTask
+    );
+
+    Parallel.Invoke(() => Console.Write(""a""));
+  }
+}
+";
+      await Verifier.VerifyAnalyzerAsync(input, true);
+    }
+
+    [Test]
+    public async Task SafeContext_WithTasksParallelAndSeveralLocalUsingDeclarations2_NoDiagnostic ()
+    {
+      const string input = @"
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Linq;
+using Remotion.Context;
+public class A
+{
+  public void Test ()
+  {
+    using var reader = File.OpenText(""notAFile"");
+    using var boundary = SafeContext.Parallel.OpenSafeContextBoundary();
+
+    SafeContext.Instance.SetData(""a"", 3);
+
+    Parallel.For(0, 5, i => { i = (int) SafeContext.Instance.GetData(""a"");});
+
+    Parallel.ForEach(Enumerable.Range(5, 10).ToList(), i => Console.Write(i));
+
+    Parallel.ForEachAsync(
+        Enumerable.Range(0, 5),
+        new ParallelOptions(), 
+        (i, token) => ValueTask.CompletedTask
+    );
+
+    Parallel.Invoke(() => Console.Write(""a""));
+  }
+}
+";
+      await Verifier.VerifyAnalyzerAsync(input, true);
+    }
+
+    [Test]
+    public async Task SafeContext_WithTasksParallelAndSafeContextBoundaryBlock_NoDiagnostic ()
+    {
+      const string input = @"
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using Remotion.Context;
+public class A
+{
+  public void Test ()
+  {
+    using (SafeContext.Parallel.OpenSafeContextBoundary()) 
+    {
+      SafeContext.Instance.SetData(""a"", 3);
+ 
+      Parallel.For(0, 5, i => { i = (int) SafeContext.Instance.GetData(""a"");});
+
+      Parallel.ForEach(Enumerable.Range(5, 10).ToList(), i => Console.Write(i));
+
+      Parallel.ForEachAsync(
+          Enumerable.Range(0, 5),
+          new ParallelOptions(), 
+          (i, token) => ValueTask.CompletedTask
+      );
+
+      Parallel.Invoke(() => Console.Write(""a""));
+    }
+  }
+}
+";
+      await Verifier.VerifyAnalyzerAsync(input, true);
+    }
+
+    [Test]
+    public async Task SafeContext_WithTasksParallelAndSafeContextBoundaryInstanceBlock_NoDiagnostic ()
+    {
+      const string input = @"
+using System;
+using System.Threading.Tasks;
+using System.Linq;
+using Remotion.Context;
+public class A
+{
+  public void Test ()
+  {
+    using (SafeContext.Instance.OpenSafeContextBoundary()) 
+    {
+      SafeContext.Instance.SetData(""a"", 3);
+ 
+      Parallel.For(0, 5, i => { i = (int) SafeContext.Instance.GetData(""a"");});
+
+      Parallel.ForEach(Enumerable.Range(5, 10).ToList(), i => Console.Write(i));
+
+      Parallel.ForEachAsync(
+          Enumerable.Range(0, 5),
+          new ParallelOptions(), 
+          (i, token) => ValueTask.CompletedTask
+      );
+
+      Parallel.Invoke(() => Console.Write(""a""));
+    }
+  }
+}
+";
+      await Verifier.VerifyAnalyzerAsync(input, true);
+    }
+
+    [Test]
+    public async Task SafeContext_WithTasksParallelAndNestedSafeContextBoundaryBlock_NoDiagnostic ()
+    {
+      const string input = @"
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Linq;
+using Remotion.Context;
+public class A
+{
+  public void Test ()
+  {
+    using (SafeContext.Parallel.OpenSafeContextBoundary()) 
+    {
+      using (var reader = File.OpenText(""notAFile.txt""))
+      {
+        SafeContext.Instance.SetData(""a"", 3);
+   
+        Parallel.For(0, 5, i => { i = (int) SafeContext.Instance.GetData(""a"");});
+
+        Parallel.ForEach(Enumerable.Range(5, 10).ToList(), i => Console.Write(i));
+
+        Parallel.ForEachAsync(
+            Enumerable.Range(0, 5),
+            new ParallelOptions(), 
+            (i, token) => ValueTask.CompletedTask
+        );
+
+        Parallel.Invoke(() => Console.Write(""a""));
+      }
+    }
+  }
+}
+";
+      await Verifier.VerifyAnalyzerAsync(input, true);
+    }
+
+    [Test]
     public async Task SafeContext_NewThread_Diagnostic ()
     {
       const string input = @"
