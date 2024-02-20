a09abe24702bd1523689a950da4b27c33d7a3fc6
Patrick Widauer <patrick.widauer@rubicon.eu>
2024-02-20 14:42:22 +0100

    RM-9093 Add FakeCacheableResourcePathBuilder

---
+++ Remotion/Development/Web/UnitTesting/Resources/FakeCacheableResourcePathBuilder.cs
@@ -0,0 +1,44 @@
+﻿// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using Remotion.Web.Resources;
+
+namespace Remotion.Development.Web.UnitTesting.Resources
+{
+  /// <summary>
+  /// Fake implementation of the <see cref="ICacheableResourcePathBuilder"/> interface, intended for use in unit testing.
+  /// </summary>
+  public class FakeCacheableResourcePathBuilder : ResourcePathBuilderBase, ICacheableResourcePathBuilder
+  {
+    public FakeCacheableResourcePathBuilder ()
+        : base(new FakeStaticResourceCacheKeyProvider())
+    {
+    }
+
+    /// <inheritdoc />
+    protected override string GetResourceRoot ()
+    {
+      return "/fake";
+    }
+
+    /// <inheritdoc />
+    protected override string BuildPath (string[] completePath)
+    {
+      return string.Join("/", completePath);
+    }
+  }
+}
