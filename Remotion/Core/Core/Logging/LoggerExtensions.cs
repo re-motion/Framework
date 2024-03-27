f4cff19764c0a6bc2ec616c7f7ac2c564970df3b
Joel Kudiyirickal <joel.kudiyirickal@rubicon.eu>
2024-03-27 12:51:16 +0100

    RM-9117 Upgrade to new logging infrastructure

---
+++ Remotion/Core/Core/Logging/LoggerExtensions.cs
@@ -0,0 +1,77 @@
+// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+// 
+// The re-motion Core Framework is free software; you can redistribute it 
+// and/or modify it under the terms of the GNU Lesser General Public License 
+// as published by the Free Software Foundation; either version 2.1 of the 
+// License, or (at your option) any later version.
+// 
+// re-motion is distributed in the hope that it will be useful, 
+// but WITHOUT ANY WARRANTY; without even the implied warranty of 
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the 
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+// 
+using System;
+using System.Collections.Generic;
+using MicrosoftLogger = Microsoft.Extensions.Logging.ILogger;
+using MicrosoftLoglevel = Microsoft.Extensions.Logging.LogLevel;
+
+namespace Remotion.Logging
+{
+  /// <summary>
+  /// Provides extension methods used for logging.
+  /// </summary>
+  public static class LoggerExtensions
+  {
+    /// <summary>
+    /// Logs the given value and returns it to the caller. This is typically used to logger a value returned by a method directly in the return
+    /// statement.
+    /// </summary>
+    /// <typeparam name="T">The (inferred) type of the value to be logged.</typeparam>
+    /// <param name="value">The value to be logged.</param>
+    /// <param name="logger">The <see cref="MicrosoftLogger"/> to logger the value with.</param>
+    /// <param name="logLevel">The <see cref="MicrosoftLoglevel"/> to logger the value at. If the <paramref name="logger"/> does not support this level, the
+    /// <paramref name="messageCreator"/> is not called.</param>
+    /// <param name="messageCreator">A function object building the message to be logged.</param>
+    /// <returns>The <paramref name="value"/> passed in to the method.</returns>
+    public static T LogAndReturnValue<T> (this T value, MicrosoftLogger logger, MicrosoftLoglevel logLevel, Func<T, string?> messageCreator)
+    {
+      if (logger.IsEnabled(logLevel))
+      {
+        logger.Log(logLevel, messageCreator(value));
+      }
+
+      return value;
+    }
+
+    public static IEnumerable<T> LogAndReturnItems<T> (
+        this IEnumerable<T> sequence,
+        MicrosoftLogger logger,
+        MicrosoftLoglevel logLevel,
+        Func<int, string?> iterationCompletedMessageCreator)
+    {
+      if (logger.IsEnabled(logLevel))
+        return LogAndReturnWithIteration(sequence, logger, logLevel, iterationCompletedMessageCreator);
+      return sequence;
+    }
+
+    private static IEnumerable<T> LogAndReturnWithIteration<T> (
+        IEnumerable<T> sequence,
+        MicrosoftLogger logger,
+        MicrosoftLoglevel logLevel,
+        Func<int, string?> iterationCompletedMessageCreator)
+    {
+      int count = 0;
+      foreach (var item in sequence)
+      {
+        count++;
+        yield return item;
+      }
+
+      logger.Log(logLevel, iterationCompletedMessageCreator(count));
+    }
+  }
+}
---
+++ Remotion/Core/Logging.Log4Net/Log4NetLogger.cs
@@ -0,0 +1,135 @@
+﻿// This file is part of the re-motion Core Framework (www.re-motion.org)
+// Copyright (c) rubicon IT GmbH, www.rubicon.eu
+//
+// The re-motion Core Framework is free software; you can redistribute it
+// and/or modify it under the terms of the GNU Lesser General Public License
+// as published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// re-motion is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with re-motion; if not, see http://www.gnu.org/licenses.
+//
+using System;
+using System.Globalization;
+using log4net.Core;
+using Remotion.Utilities;
+
+using MicrosoftLogger = Microsoft.Extensions.Logging.ILogger;
+using MicrosoftLoglevel = Microsoft.Extensions.Logging.LogLevel;
+using MicrosoftEventId = Microsoft.Extensions.Logging.EventId;
+using RemotionLoglevel = Remotion.Logging.LogLevel;
+
+namespace Remotion.Logging.Log4Net;
+
+/// <summary>
+/// Implements <see cref="Microsoft.Extensions.Logging.ILogger"/> to provide an implementation with log4net.
+/// </summary>
+public class Log4NetLogger : MicrosoftLogger
+{
+  public static Level Convert (MicrosoftLoglevel logLevel)
+  {
+    return logLevel switch
+    {
+        MicrosoftLoglevel.Trace => Level.Trace,
+        MicrosoftLoglevel.Debug => Level.Debug,
+        MicrosoftLoglevel.Information => Level.Info,
+        MicrosoftLoglevel.Warning => Level.Warn,
+        MicrosoftLoglevel.Error => Level.Error,
+        MicrosoftLoglevel.Critical => Level.Critical,
+        MicrosoftLoglevel.None => Level.Off,
+        _ => throw new ArgumentException(string.Format("LogLevel does not support value {0}.", logLevel), "logLevel")
+    };
+  }
+
+  public static Level Convert (RemotionLoglevel logLevel)
+  {
+    return logLevel switch
+    {
+        RemotionLoglevel.Debug => Level.Debug,
+        RemotionLoglevel.Info => Level.Info,
+        RemotionLoglevel.Warn => Level.Warn,
+        RemotionLoglevel.Error => Level.Error,
+        RemotionLoglevel.Fatal => Level.Fatal,
+        _ => throw new ArgumentException(string.Format("LogLevel does not support value {0}.", logLevel), "logLevel")
+    };
+  }
+
+  public log4net.Core.ILogger Logger { get; }
+
+  public Log4NetLogger (ILogger logger)
+  {
+    ArgumentUtility.CheckNotNull("logger", logger);
+
+    Logger = logger;
+  }
+
+  public void Log<TState> (MicrosoftLoglevel logLevel, MicrosoftEventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
+  {
+    var level = Convert(logLevel);
+    if (Logger.IsEnabledFor(level))
+      Logger.Log(CreateLoggingEvent(level, eventId.Id, formatter(state, exception), exception));
+  }
+
+  public bool IsEnabled (MicrosoftLoglevel logLevel)
+  {
+    return Logger.IsEnabledFor(Convert(logLevel));
+  }
+
+  public IDisposable? BeginScope<TState> (TState state)
+      where TState : notnull
+  {
+    return null;
+  }
+
+  private LoggingEvent CreateLoggingEvent (Level level, int? eventID, string message, Exception? exceptionObject)
+  {
+    var loggingEvent = new LoggingEvent(typeof(Log4NetLogger), null, Logger.Name, level, message, exceptionObject);
+
+    if (eventID.HasValue)
+    {
+      if (eventID < 0 || eventID > 0xFFFF)
+      {
+        LogLoggingError(eventID.Value, exceptionObject, message);
+
+        throw new ArgumentOutOfRangeException(
+            "eventID", string.Format("An event id of value {0} is not supported. Valid event ids must be within a range of 0 and 65535.", eventID));
+      }
+
+      loggingEvent.Properties["EventID"] = eventID;
+    }
+
+    return loggingEvent;
+  }
+
+  private void LogLoggingError (int eventID, Exception? exceptionObject, string message)
+  {
+    int safeEventID;
+    if (eventID < 0)
+      safeEventID  = 0;
+    else if (eventID > 0xFFFF)
+      safeEventID  = 0xFFFF;
+    else
+      safeEventID = eventID;
+
+    Level level = Level.Error;
+
+    if (Logger.IsEnabledFor(level))
+    {
+      Logger.Log(
+          CreateLoggingEvent(
+              level,
+              safeEventID,
+              string.Format(
+                  CultureInfo.InvariantCulture,
+                  "Failure during logging of message:\r\n{0}\r\nEvent ID: {1}",
+                  // TODO RM-7803: message or the ToString() result being null should result in a fallback value being used.
+                  new object?[] { message!.ToString(), eventID }),
+              exceptionObject));
+    }
+  }
+}
