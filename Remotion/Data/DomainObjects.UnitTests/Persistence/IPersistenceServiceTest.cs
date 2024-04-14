35172857e1868235242a3f8bbcaae19ec2a4d727
Michael Ketting <michael.ketting@rubicon.eu>
2024-04-14 18:45:41 +0200

    f RM-9122 Rename PersistenceManager to PersistenceService

--- Remotion/Data/DomainObjects.UnitTests/Persistence/IPersistenceServiceTest.cs
+++ Remotion/Data/DomainObjects.UnitTests/Persistence/IPersistenceServiceTest.cs
@@ -35,19 +35,19 @@ namespace Remotion.Data.DomainObjects.UnitTests.Persistence
     [Test]
     public void GetInstance_Once ()
     {
-      var factory = _serviceLocator.GetInstance<IPersistenceService>();
+      var service = _serviceLocator.GetInstance<IPersistenceService>();
 
-      Assert.That(factory, Is.Not.Null);
-      Assert.That(factory, Is.TypeOf(typeof(PersistenceService)));
+      Assert.That(service, Is.Not.Null);
+      Assert.That(service, Is.TypeOf(typeof(PersistenceService)));
     }
 
     [Test]
     public void GetInstance_Twice_ReturnsSameInstance ()
     {
-      var factory1 = _serviceLocator.GetInstance<IPersistenceService>();
-      var factory2 = _serviceLocator.GetInstance<IPersistenceService>();
+      var service1 = _serviceLocator.GetInstance<IPersistenceService>();
+      var service2 = _serviceLocator.GetInstance<IPersistenceService>();
 
-      Assert.That(factory1, Is.SameAs(factory2));
+      Assert.That(service1, Is.SameAs(service2));
     }
   }
 }
