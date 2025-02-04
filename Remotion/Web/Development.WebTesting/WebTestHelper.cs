d5fb7490d465cdfb9745f79a86617f412f61c6f8
Michael Ludwig <michael.ludwig1392@gmail.com>
2025-02-04 13:35:20 +0100

    webtest teardown do not make desktop screenshot

--- Remotion/Web/Development.WebTesting/WebTestHelper.cs
+++ Remotion/Web/Development.WebTesting/WebTestHelper.cs
@@ -299,9 +299,6 @@ namespace Remotion.Web.Development.WebTesting
       {
         Assertion.IsNotNull(_testName, "'{0}' should be set by the test infrastructure calling '{1}'", nameof(_testName), nameof(OnSetUp));
         var screenshotRecorder = new TestExecutionScreenshotRecorder(_testInfrastructureConfiguration.ScreenshotDirectory, _loggerFactory);
-        screenshotRecorder.CaptureCursor();
-        if (_mainBrowserSession is { Headless: false })
-          screenshotRecorder.TakeDesktopScreenshot(_testName);
         screenshotRecorder.TakeBrowserScreenshot(_testName, _browserSessions.ToArray(), BrowserConfiguration.Locator);
       }
 
