96c619549a14cff8efe8783d85c37274616ac3aa
Michael Ludwig <michael.ludwig1392@gmail.com>
2025-02-06 15:37:51 +0100

    move compiler flag below licence

--- Remotion/Web/Development.WebTesting/ScreenshotCreation/WindowsCursorInformation.cs
+++ Remotion/Web/Development.WebTesting/ScreenshotCreation/WindowsCursorInformation.cs
@@ -1,5 +1,4 @@
-﻿#if PLATFORM_WINDOWS
-// This file is part of the re-motion Core Framework (www.re-motion.org)
+﻿// This file is part of the re-motion Core Framework (www.re-motion.org)
 // Copyright (c) rubicon IT GmbH, www.rubicon.eu
 // 
 // The re-motion Core Framework is free software; you can redistribute it 
@@ -15,6 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with re-motion; if not, see http://www.gnu.org/licenses.
 // 
+#if PLATFORM_WINDOWS
 using System;
 using System.ComponentModel;
 using System.Drawing;
