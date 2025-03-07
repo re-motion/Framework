82c6d1736ae8cceaac2e0eab00cb4f876b65074d
Martin Herwich <martin.herwich@rubicon.eu>
2025-03-07 16:23:57 +0100

    RM-9438 Web.WebTesting.IntegrationTests.RequireUI with browser log checking

--- Remotion/Web/Development.WebTesting.IntegrationTests.RequireUI/SetUpFixture.cs
+++ Remotion/Web/Development.WebTesting.IntegrationTests.RequireUI/SetUpFixture.cs
@@ -20,8 +20,15 @@ using log4net.Config;
 using Microsoft.Extensions.Logging;
 using NUnit.Framework;
 using Remotion.Logging.Log4Net;
+using Remotion.Web.Development.WebTesting.BrowserLog;
 using Remotion.Web.Development.WebTesting.Configuration;
 using Remotion.Web.Development.WebTesting.IntegrationTests.Infrastructure;
+
+[assembly: PerformBrowserLogCheck(true)]
+[assembly: BrowserLogMinimumLevel(OpenQA.Selenium.LogLevel.Warning)]
+[assembly: IgnoreBrowserLogMessage(ExpectedBrowserLogMessages.FavIconNotFound)]
+[assembly: IgnoreBrowserLogMessage(ExpectedBrowserLogMessages.FontNotUsed)]
+[assembly: IgnoreBrowserLogMessage(ExpectedBrowserLogMessages.BeforeUnloadAlertBlocked)]
 
 namespace Remotion.Web.Development.WebTesting.IntegrationTests
 {
