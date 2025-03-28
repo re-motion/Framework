8ca1fdfb3598c4f690ec9121689806d518b55650
Martin Herwich <martin.herwich@rubicon.eu>
2025-03-28 18:04:48 +0100

    RM-9466 Activate CSP in WebTesting-TestSites

--- Remotion/ObjectBinding/Web.Development.WebTesting.TestSite.Shared/ContentSecurityPolicy/TestCspDefaultsProvider.cs
+++ Remotion/ObjectBinding/Web.Development.WebTesting.TestSite.Shared/ContentSecurityPolicy/TestCspDefaultsProvider.cs
@@ -1,12 +1,13 @@
 // SPDX-FileCopyrightText: (c) RUBICON IT GmbH, www.rubicon.eu
 // SPDX-License-Identifier: LGPL-2.1-or-later
+using System;
 using Remotion.ServiceLocation;
 using Remotion.Web.ContentSecurityPolicy;
 using Remotion.Web.UI;
 
-namespace Remotion.Web.Test.Shared;
+namespace Remotion.ObjectBinding.Web.Development.WebTesting.TestSite.Shared.ContentSecurityPolicy;
 
-[ImplementationFor(typeof(ICspDefaultsProvider), Lifetime = LifetimeKind.Singleton, Position = CspDefaultsProvider.Position + 1)]
+[ImplementationFor(typeof(ICspDefaultsProvider), Lifetime = LifetimeKind.Singleton, Position = CspDefaultsProvider.Position -1)]
 public class TestCspDefaultsProvider : ICspDefaultsProvider
 {
   public bool GetDefaultIsCspEnabledForPage (IPage page)
@@ -16,16 +17,15 @@ public class TestCspDefaultsProvider : ICspDefaultsProvider
 
   public bool GetDefaultIsCspReportOnlyEnabledForPage (IPage page)
   {
-    return true;
+    return false;
   }
 
   public CspHeader GetDefaultCspHeaderForPage (IPage page)
   {
-    return CspDefaultsProvider.DefaultCspHeader;
+    return CspDefaultsProvider.DefaultCspHeader.AddDirectiveValue(CspDirectives.ScriptSrc, "'nonce-testsite'");
   }
-
   public CspHeader GetDefaultCspReportOnlyHeaderForPage (IPage page)
   {
-    return CspDefaultsProvider.DefaultCspHeader;
+    return CspDefaultsProvider.DefaultCspHeader.AddDirectiveValue(CspDirectives.ScriptSrc, "'nonce-testsite'");
   }
 }
