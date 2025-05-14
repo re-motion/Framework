5f00f06f24474dd30cd0b5b8894cc0b921b7f7f0
Patrick Widauer <patrick.widauer@rubicon.eu>
2025-05-14 10:25:00 +0200

    RM-9512 Start SQL Server Docker images are part of the build

---
+++ Build/Customizations/DatabaseTestResourceFactory.cs
@@ -0,0 +1,54 @@
+﻿// SPDX-FileCopyrightText: (c) RUBICON IT GmbH, www.rubicon.eu
+// SPDX-License-Identifier: LGPL-2.1-or-later
+using System;
+using System.Linq;
+using Customizations.SqlServer;
+using Remotion.BuildScript;
+using Remotion.BuildScript.Test;
+
+namespace Customizations;
+
+public class DatabaseTestResourceFactory : ITestResourceFactory
+{
+  private const string c_imageNameParameter = "ImageName";
+  private const string c_hostNameParameter = "HostName";
+
+  public Databases Database { get; }
+
+  public string Name => $"SQL Server {(Database.HasSpecificVersion ? Database.Version : "(default)")}";
+
+  public DatabaseTestResourceFactory (Databases database)
+  {
+    Database = database;
+  }
+
+  public void ConfigureTestParameters (TestParameterBuilder builder)
+  {
+    builder.AddOptionalParameter(Database, c_imageNameParameter, "");
+    builder.AddOptionalParameter(Database, c_hostNameParameter, $"{Database}HostName");
+  }
+
+  public void Start (TestResourceFactoryContext context)
+  {
+    if (Database == Databases.NoDB)
+      return;
+
+    var imageName = context.TestParameters.GetTestParameter(Database, c_imageNameParameter);
+    var hostName = context.TestParameters.GetTestParameter(Database, c_hostNameParameter);
+
+    if (string.IsNullOrEmpty(imageName))
+      return;
+
+    var network = context.TestResources
+        .OfType<DockerNetworkResource>()
+        .SingleOrDefault()
+        ?.Network;
+
+    ISqlDockerImage sqlServerDockerImage = OperatingSystem.IsWindows()
+        ? new WindowsSqlServerDockerImage(imageName, hostName, network)
+        : new LinuxSqlServerDockerImage(imageName, hostName, network);
+
+    var databaseTestResource = new DatabaseTestResource(Name, Database, sqlServerDockerImage);
+    context.TestResources.Add(databaseTestResource);
+  }
+}
---
+++ Build/Customizations/SqlServer/ISqlDockerImage.cs
@@ -0,0 +1,10 @@
+﻿// SPDX-FileCopyrightText: (c) RUBICON IT GmbH, www.rubicon.eu
+// SPDX-License-Identifier: LGPL-2.1-or-later
+using System;
+
+namespace Customizations.SqlServer;
+
+public interface ISqlDockerImage : IDisposable
+{
+  string GetConnectionString ();
+}
