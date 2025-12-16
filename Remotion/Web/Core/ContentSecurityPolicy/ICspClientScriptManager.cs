a0214816fdacecdc3b837ab5a575a1cb413f036e
Patrick Widauer <patrick.widauer@rubicon.eu>
2025-12-16 08:57:49 +0100

    RM-9634 Loosen ISmartPage dependency for CspEnabledHtmlTextWriter

---
+++ Remotion/Web/Core/ContentSecurityPolicy/ICspClientScriptManager.cs
@@ -0,0 +1,15 @@
+﻿// SPDX-FileCopyrightText: (c) RUBICON IT GmbH, www.rubicon.eu
+// SPDX-License-Identifier: LGPL-2.1-or-later
+namespace Remotion.Web.ContentSecurityPolicy;
+
+/// <summary>
+/// Abstraction used by <see cref="CspEnabledHtmlTextWriter"/> to allow different ways to register CSP client scripts.
+/// </summary>
+public interface ICspClientScriptManager
+{
+  /// <summary>
+  /// Registers the specified <paramref name="script"/> using the specified <paramref name="key"/>.
+  /// If the key already exists, a previously registered script will be overriden.
+  /// </summary>
+  void RegisterScript (string key, string script);
+}
---
+++ Remotion/Web/Core/ContentSecurityPolicy/SmartPageCspClientScriptManagerAdapter.cs
@@ -0,0 +1,31 @@
+﻿// SPDX-FileCopyrightText: (c) RUBICON IT GmbH, www.rubicon.eu
+// SPDX-License-Identifier: LGPL-2.1-or-later
+using System;
+using Remotion.Web.UI;
+
+namespace Remotion.Web.ContentSecurityPolicy;
+
+/// <summary>
+/// Adapter for <see cref="ICspClientScriptManager"/> to work with <see cref="ISmartPage"/>.
+/// Registers scripts using smartPage.ClientScript.RegisterStartupScriptBlock.
+/// </summary>
+public class SmartPageCspClientScriptManagerAdapter : ICspClientScriptManager
+{
+  private readonly ISmartPage _smartPage;
+
+  public SmartPageCspClientScriptManagerAdapter (ISmartPage smartPage)
+  {
+    ArgumentNullException.ThrowIfNull(smartPage);
+
+    _smartPage = smartPage;
+  }
+
+  /// <inheritdoc />
+  public void RegisterScript (string key, string script)
+  {
+    ArgumentException.ThrowIfNullOrEmpty(key);
+    ArgumentException.ThrowIfNullOrEmpty(script);
+
+    _smartPage.ClientScript.RegisterStartupScriptBlock(_smartPage, typeof(CspEnabledHtmlTextWriter), key, script);
+  }
+}
