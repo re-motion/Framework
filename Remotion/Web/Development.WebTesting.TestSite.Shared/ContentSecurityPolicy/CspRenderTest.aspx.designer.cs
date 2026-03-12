5c35bbf6ea2b9369a2ecf8b0bd3cb891678dde8f
Patrick Widauer <patrick.widauer@rubicon.eu>
2026-03-12 12:23:11 +0100

    RM-9656 Fix dispose scripts using eval when CSP is enabled

--- Remotion/Web/Development.WebTesting.TestSite.Shared/ContentSecurityPolicy/CspRenderTest.aspx.designer.cs
+++ Remotion/Web/Development.WebTesting.TestSite.Shared/ContentSecurityPolicy/CspRenderTest.aspx.designer.cs
@@ -13,5 +13,14 @@ namespace Remotion.Web.Development.WebTesting.TestSite.Shared.ContentSecurityPol
 
     public partial class CspRenderTest
     {
+
+        /// <summary>
+        /// form1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
     }
 }
